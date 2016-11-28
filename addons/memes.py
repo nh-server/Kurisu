0977ca9b19a848d5ae88c7a4c25d6a0437c226b1
notrankin <trevor.roberson19@gmail.com>
2016-11-28 03:34:40 -0500

    Help (#1)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete memes.py
    
    * Add files via upload
    
    * Add files via upload
    
    * Update memes.py

--- addons/memes.py
+++ addons/memes.py
@@ -61,11 +61,17 @@ class Memes:
     async def permabrocked(self, ctx):
         """Memes."""
         await self.bot.say(self.check_channel(ctx, "http://i.imgur.com/ARsOh3p.jpg"))
-
+		
+	
     @commands.command(pass_context=True)
     async def clap(self, ctx):
         """Memes."""
         await self.bot.say(self.check_channel(ctx, "http://i.imgur.com/UYbIZYs.gifv"))
+		
+    @commands.command(pass_context=True)
+    async def hazel(self, ctx):
+        """Memes."""
+        await self.bot.say(self.check_channel(ctx, "http://i.imgur.com/vpu8bX3.png"))
 
     # Cute commands :3
     @commands.command(pass_context=True)
