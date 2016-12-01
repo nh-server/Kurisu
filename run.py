aa2649812cea9db0d670607c9731af147a894e86
916253 <f916253@me.com>
2016-12-01 16:44:27 -0500

    Set working directory to Kurisu's folder
    Makes sure that the working directory is set to Kurisu's location so that the config is properly read even when executed from elsewhere.

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ A bot being slowly written to replace Saber as Saber is no longer actively devel
 """
 
 # import dependencies
+import os
 from discord.ext import commands
 import discord
 import datetime, re
@@ -20,6 +21,10 @@ import traceback
 import sys
 import os
 from collections import Counter
+
+# sets working directory to bot's folder
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
 
 # read config for token
 config = configparser.ConfigParser()
@@ -35,7 +40,7 @@ async def on_ready():
         await bot.send_message(discord.utils.get(server.channels, name="mods"), "{} has started!".format(bot.user.name))
         break
 
-# outputs all messages to a log file
+# outputs errors to a log file, clears every run to save space
 logger = logging.getLogger('discord')
 logger.setLevel(logging.ERROR)
 handler = logging.FileHandler(filename='numberbot_output.log', encoding='utf-8', mode='w')
@@ -61,4 +66,5 @@ for extension in addons:
         print('The addon failed to load! {}\n{}: {}'.format(extension, type(e).__name__, e))
 
 # Execute
+print('Bot directory:', dir_path)
 bot.run(config['Main']['token'])
