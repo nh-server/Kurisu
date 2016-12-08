0ab0f833322b68f6fa7c2fa8c06f2063a483d0d1
ihaveamac <ian@ianburgwin.net>
2016-12-08 08:24:59 -0800

    embeds for ninerr

--- addons/ninerr.py
+++ addons/ninerr.py
@@ -15,8 +15,8 @@ class NinErr:
         '005-5602': 'Unable to connect to the eShop. This error is most likely the result of an incorrect region setting.\nMake sure your region is correctly set in System Settings. If you encounter this error after region-changing your system, make sure you followed all the steps properly.',
         '005-5964': 'Your Nintendo Network ID has been banned from accessing the eShop.\nIf you think this was unwarranted, you will have to contact Nintendo Support to have it reversed.',
         '007-2001': 'Usually the result after region-changing the system. New 3DS cannot fix this issue right now.',
-        '007-2100': 'The connection to the Nintendo eShop timed out. \nThis may be due to an ongoing server maintenance, check <https://support.nintendo.com/networkstatus> to make sure the servers are operating normally. You may also encounter this error if you have a weak internet connection.',
-        '007-2404': 'An error occurred while attempting to connect to the Nintendo eShop. \nMake sure you are running the latest firmware, since this error will appear if you are trying to access the eShop on older versions.',
+        '007-2100': 'The connection to the Nintendo eShop timed out.\nThis may be due to an ongoing server maintenance, check <https://support.nintendo.com/networkstatus> to make sure the servers are operating normally. You may also encounter this error if you have a weak internet connection.',
+        '007-2404': 'An error occurred while attempting to connect to the Nintendo eShop.\nMake sure you are running the latest firmware, since this error will appear if you are trying to access the eShop on older versions.',
         '007-2923': 'The Nintendo Servers are currently down for maintenance. Please try again later.',
         '007-3102': 'Cannot find title on Nintendo eShop. Probably pulled.',
         '011-3021': 'Cannot find title on Nintendo eShop. Probably incorrect region, or never existed.',
@@ -38,12 +38,14 @@ class NinErr:
         if len(err) != 8 or err[3] != "-":
             await self.bot.say("Wrong format. Error codes use the format `XXX-YYYY`.")
             return
-        result = "**{}**\n".format(err)
+        embed = discord.Embed(title=err, color=discord.Color.dark_red())
+        embed.url = "http://www.nintendo.com/consumer/wfc/en_na/ds/results.jsp?error_code={}&system=3DS&locale=en_US".format(err)
+        #result = "**{}**\n".format(err)
         if err not in self.errcodes:
-            result += "I don't know this one! Try this page: <http://www.nintendo.com/consumer/wfc/en_na/ds/results.jsp?error_code={}&system=3DS&locale=en_US>".format(err)
+            embed.description = "I don't know this one! Click the error code for details on Nintendo Support."
         else:
-            result += self.errcodes[err]
-        await self.bot.say(result)
+            embed.description = self.errcodes[err]
+        await self.bot.say("", embed=embed)
 
 def setup(bot):
     bot.add_cog(NinErr(bot))
