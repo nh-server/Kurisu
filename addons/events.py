fa161d5bf3e863bdbfa0724c9ff121a6a60b6df1
ihaveamac <ian@ianburgwin.net>
2017-01-02 14:42:27 -0800

    check all freeshop urls on notabug

--- addons/events.py
+++ addons/events.py
@@ -17,10 +17,10 @@ class Events:
         msg = message.content.lower()
         contains_invite_link = "discordapp.com/invite" in msg or "discord.gg" in msg
         # special check for a certain thing
-        contains_fs_url = "notabug.org/circuit-unbroken/freeshop" in msg
+        contains_fs_url = re.match('(.*)notabug\.org\/(.*)\/freeshop(.*)', msg, re.IGNORECASE)
         if contains_invite_link:
             await self.bot.send_message(self.bot.mods_channel, "✉️ **Invite posted**: {} posted an invite link in {}".format(message.author.mention, message.channel.mention))
-        if contains_fs_url:
+        if contains_fs_url != None:
             await self.bot.delete_message(message)
             await self.bot.send_message(self.bot.mods_channel, "**Bad URL**: {} posted a freeShop URL in {} (message deleted)".format(message.author.mention, message.channel.mention))
 
