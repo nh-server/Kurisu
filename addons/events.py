19b6b066f58616c053066786bd36978ecf43ac66
ihaveamac <ian@ianburgwin.net>
2017-01-03 03:02:47 -0800

    show msg when deleting due to certain sites

--- addons/events.py
+++ addons/events.py
@@ -21,12 +21,14 @@ class Events:
         contains_piracy_site_mention = any(x in msg for x in ('3dsiso', '3dschaos', 'iso site', 'chaos site'))
         if contains_invite_link:
             await self.bot.send_message(self.bot.mods_channel, "✉️ **Invite posted**: {} posted an invite link in {}".format(message.author.mention, message.channel.mention))
+        embed = discord.Embed()
+        embed.description = message.content
         if contains_fs_repo_url != None:
             await self.bot.delete_message(message)
-            await self.bot.send_message(self.bot.mods_channel, "**Bad URL**: {} posted a freeShop Repo URL in {} (message deleted)".format(message.author.mention, message.channel.mention))
+            await self.bot.send_message(self.bot.mods_channel, "**Bad URL**: {} posted a freeShop Repo URL in {} (message deleted)".format(message.author.mention, message.channel.mention), embed=embed)
         if contains_piracy_site_mention:
             await self.bot.delete_message(message)
-            await self.bot.send_message(self.bot.mods_channel, "**Bad site**: {} mentioned a piracy site in {} (message deleted)".format(message.author.mention, message.channel.mention))
+            await self.bot.send_message(self.bot.mods_channel, "**Bad site**: {} mentioned a piracy site in {} (message deleted)".format(message.author.mention, message.channel.mention), embed=embed)
 
     async def on_message(self, message):
         await self.scan_message(message)
