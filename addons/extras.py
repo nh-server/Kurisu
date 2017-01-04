a7d4deb46ab2c1d2f1ccdc1ce94cc89ddc152dd6
ihaveamac <ian@ianburgwin.net>
2017-01-03 19:23:12 -0800

    dumpchannel

--- addons/extras.py
+++ addons/extras.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 from sys import argv
 
@@ -24,6 +25,18 @@ class Extras:
     async def membercount(self):
         """Prints the member count of the server."""
         await self.bot.say("{} has {} members!".format(self.bot.server.name, self.bot.server.member_count))
+
+    @commands.has_permissions(administrator=True)
+    @commands.command(pass_context=True, hidden=True)
+    async def dumpchannel(self, ctx, channel_name, limit=100):
+        """Dump 100 messages from a channel to a file."""
+        channel = ctx.message.channel_mentions[0]
+        await self.bot.say("Dumping {} messages from {}".format(limit, channel.mention))
+        os.makedirs("#{}-{}".format(channel.name, channel.id), exist_ok=True)
+        async for message in self.bot.logs_from(channel, limit=limit):
+            with open("#{}-{}/{}.txt".format(channel.name, channel.id, message.id), "w") as f:
+                f.write(message.content)
+        await self.bot.say("Done!")
 
 def setup(bot):
     bot.add_cog(Extras(bot))
