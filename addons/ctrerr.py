1756de722e8eb4ab442a2adff42b35097a6a3b8e
ihaveamac <ian@ianburgwin.net>
2017-01-05 03:17:36 -0800

    remove unused err2

--- addons/ctrerr.py
+++ addons/ctrerr.py
@@ -225,25 +225,5 @@ class CTRErr:
         result += self.get_name(self.levels, level)
         await self.bot.say(result)
 
-    @commands.command(pass_context=True)
-    async def err2(self, ctx, err: str):
-        """Parses CTR error codes, with a fancy embed (TEST). 0x prefix is not required. \n Example: .err 0xD960D02B"""
-        err = err.strip()
-        if err.startswith("0x"):
-            err = err[2:]
-        rc = int(err, 16)
-        desc  = rc & 0x3FF
-        mod   = (rc >> 10) & 0xFF
-        summ  = (rc >> 21) & 0x3F
-        level = (rc >> 27) & 0x1F
-
-        # garbage
-        embed = discord.Embed(title="0x{:X}".format(rc))
-        embed.add_field(name="Module", value=self.get_name(self.modules, mod), inline=False)
-        embed.add_field(name="Description", value=self.get_name(self.modules, desc), inline=False)
-        embed.add_field(name="Summary", value=self.get_name(self.modules, summ), inline=False)
-        embed.add_field(name="Level", value=self.get_name(self.modules, level), inline=False)
-        await self.bot.say("", embed=embed)
-
 def setup(bot):
     bot.add_cog(CTRErr(bot))
