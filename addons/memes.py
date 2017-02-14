84fe746a385da57491f72ad187cf412744afc2ed
ihaveamac <ian@ianburgwin.net>
2017-02-13 18:39:46 -0800

    re-style listmemes

--- addons/memes.py
+++ addons/memes.py
@@ -28,9 +28,7 @@ class Memes:
         # this feels wrong...
         funcs = dir(self)
         msg = "```\n"
-        for func in funcs:
-            if func != "bot" and func[0] != "_":
-                msg += func + "\n"
+        msg += ", ".join(func for func in funcs if func != "bot" and func[0] != "_")
         msg += "```"
         await self._meme(ctx, msg)
 
@@ -39,22 +37,22 @@ class Memes:
     async def s_99(self, ctx):
         """Memes."""
         await self._meme(ctx, "**ALL HAIL BRITANNIA!**")
-        
+
     @commands.command(pass_context=True, hidden=True)
     async def adrian1(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/j0Dkv2Z.png")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def adrian2(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/2SBC1Qo.jpg")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def adrian3(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/EsDWK9U.png")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def megumi(self, ctx):
         """Memes."""
@@ -79,22 +77,22 @@ class Memes:
     async def inori4(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/267IXh1.gif")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def inori5(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/lKcsiBP.png")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def kina(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/8Mm5ZvB.jpg")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def shotsfired(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/zf2XrNk.gifv")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def r34(self, ctx):
         """Memes."""
@@ -119,27 +117,27 @@ class Memes:
     async def knp(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/EsJ191C.png")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def lucina(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/tnWSXf7.png")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def lucina2(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/ZPMveve.jpg")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def xarec(self, ctx):
         """Memes."""
         await self._meme(ctx, "https://i.imgur.com/wRVuidH.gif")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def xarec2(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/A59RbRT.png")
-    
+
     @commands.command(pass_context=True, hidden=True)
     async def clap(self, ctx):
         """Memes."""
@@ -175,12 +173,12 @@ class Memes:
     async def headpat(self, ctx):
         """Cute"""
         await self._meme(ctx, "http://i.imgur.com/7V6gIIW.jpg")
-        
+
     @commands.command(pass_context=True, hidden=True)
     async def rawr(self, ctx):
         """Cute"""
         await self._meme(ctx, "http://i.imgur.com/Bqw4OwQ.png")
-        
+
     @commands.command(pass_context=True, hidden=True)
     async def baka(self, ctx):
         """Cute"""
@@ -191,12 +189,12 @@ class Memes:
     async def led(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/FYsxaUZ.jpg")
-        
+
     @commands.command(pass_context=True, hidden=True)
     async def snickers(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/Ek0uDUn.jpg")
-        
+
 # Load the extension
 def setup(bot):
     bot.add_cog(Memes(bot))
