7deb4cba380e05cf0c2a212088dc744d3782d0af
Eevee3DS <eeveetooo@gmail.com>
2017-02-26 19:47:52 +1030

    Added .sderrors command (#69)
    * Added .sderrors command
    
    just for testing your sdcard for corruption
    
    * consistency

--- addons/assistance.py
+++ addons/assistance.py
@@ -194,6 +194,11 @@ class Assistance:
     async def bigsd(self):
         """SD bigger than 32GB"""
         await self.simple_embed("If you want to change your SD card to one bigger than 32GB then you'll have to format it to FAT32.\nYou can do this with the tool of your preference.\nFormatter examples:\n- [guiformat](http://www.ridgecrop.demon.co.uk/index.htm?guiformat.htm)\n- gparted(for Linux users)", title="Big SD cards")
+		
+    @commands.command()
+    async def sderrors(self):
+        """Sd Error Guide"""
+        await self.simple_embed("Guide For Checking SD Card For Errors\n- [H2testw Guide - Windows](https://3ds.guide/h2testw-(windows\))\n- [F3 Guide - Linux](https://3ds.guide/f3-(linux\))\n- [F3X Guide - Mac](https://3ds.guide/f3x-(mac\))", title="SD Card Errors")		
 
     @commands.command()
     async def notbricked(self):
