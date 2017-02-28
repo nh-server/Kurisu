f7147cce45f8c5ca380c2fdcdc584e587e04f06b
ihaveamac <ian@ianburgwin.net>
2017-02-28 07:54:14 -0800

    fix name

--- addons/blah.py
+++ addons/blah.py
@@ -32,13 +32,13 @@ class Blah:
     async def dm(self, ctx, channel_destination: str, *, inp):
         dest = ctx.message.mentions[0]
         await self.bot.send_message(dest, inp)
-        
+
     @commands.command()
     async def event(self):
         embed = discord.Embed(title="Event Information", description="**The 1st Nintendo Homebrew Tournament**", color=discord.Color.dark_red())
         embed.set_thumbnail(url="http://art.gametdb.com/wii/cover/US/RMCE28.png?1431288336")
         embed.set_author(name="Welcome gamers!", url="http://wiki.tockdom.com/wiki/Wiimms_Mario_Kart_Retro_2015-05", icon_url="https://wiimmfi.de/images/wiimmfi-light.svg?=64")
-        embed.set_footer(text="dubyadud & FlamFlim69", icon_url="https://cdn.discordapp.com/avatars/186483213701611520/2d1a166be1ea984a4e3d1aed1ce897b4.webp?size=64")
+        embed.set_footer(text="dubyadud & FlimFlam69", icon_url="https://cdn.discordapp.com/avatars/186483213701611520/2d1a166be1ea984a4e3d1aed1ce897b4.webp?size=64")
         embed.add_field(name="**How to Join?**", value="Wiims Mario Kart Retro is a custom distributon of Mario Kart Wii (think along the lines of a ROMhack). In order to obtain a copy of this version, users must apply a patch to their own Mario Kart Wii backups using instructions and tools found on [this page](http://wiki.tockdom.com/wiki/Wiimms_Mario_Kart_Retro_2015-05). Once that's done, you can launch the backup using your preferred system (Wii, vWii, or Dolphin) and connect online immideately. You will initially recieve an error message, but that only means you have registered with the servers and entered the 7 day activation period..")
         embed.add_field(name="**When**", value="Right now the Nintendo Homebrew discord staff, in cooperation with the Wiimmfi team, are deliberating on a date on when we can start the tourney. Users are encouraged to create their copy of Wiims Mario Kart Retro and register on the Wiimfi servers asap to ensure that the competition goes smoothely rather than widespread issues on the first day.")
         embed.add_field(name="**Assistance**", value="If you need any kind of help building your own copy of Wiims Mario Kart Retro, please refer to the #gamechat channel.")
