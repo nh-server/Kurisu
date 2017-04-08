6fe4b2bb1bb82037829c441a0dd9c24184cc8f72
pokecraft98 <aidanalmanzor@gmail.com>
2017-04-08 07:42:18 -0400

    added .vc command

--- addons/assistance.py
+++ addons/assistance.py
@@ -270,6 +270,17 @@ class Assistance:
     async def dump(self):
         """How to dump/build CIAs using GodMode9"""
         await self.bot.say("https://pastebin.com/sx8HYULr")
+          
+    #Embed to 3DS VC Injects Website
+    @commands.command()
+    async def vc(self):
+        """Link to Virtual Console Injects for 3DS"""
+        embed = discord.Embed(title="Virtual Console Injects for 3DS", color=discord.Color.blue())
+        embed.set_author(name="Asdolo", url="https://gbatemp.net/members/asdolo.389539/")
+        embed.set_thumbnail(url="https://i.imgur.com/rHa76XM.png")
+        embed.url = "https://gbatemp.net/search/40920047/?q=injector&t=post&o=date&g=1&c[title_only]=1&c[user][0]=389539"
+        embed.description = "The reccomended way to play old classics on your 3DS"
+        await self.bot.say("", embed=embed)
 
 def setup(bot):
     bot.add_cog(Assistance(bot))
