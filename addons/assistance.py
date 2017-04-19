6589dd7bd6ad9c0b7efc130708c8c8fb21f2459c
FrozenChen <brobles@umag.cl>
2017-04-19 19:47:13 -0300

    Fixed .flashcart and some typos (#147)
    * quick fix for .flashcart
    
    * Fixed typos and updated .hbl

--- addons/assistance.py
+++ addons/assistance.py
@@ -206,7 +206,7 @@ class Assistance:
     @commands.command()
     async def hbl(self):
         """Get homebrew launcher working on 11.3"""
-        await self.simple_embed("If you are encountering errors while trying to access the homebrew launcher on a 11.3 New 3DS with Luma3DS installed, you should disable the 'Clock + L2' option in the Luma3DS configuration menu (accessed by holding select while booting).\n Homebrew launcher **cant** be used on a 11.4 O3DS currently**")
+        await self.simple_embed("If you are encountering errors while trying to access the homebrew launcher on a 11.3 New 3DS with Luma3DS installed, you should disable the 'Clock + L2' option in the Luma3DS configuration menu (accessed by holding select while booting).\n Homebrew launcher **cant** be used on a stock 11.4 O3DS currently")
 
     @commands.command()
     async def readguide(self):
@@ -276,7 +276,7 @@ class Assistance:
     @commands.command()
     async def dump(self):
         """How to dump/build CIAs using GodMode9"""
-        embed = discord.Embed(title="Godmode9 dump/build Guide", color=discord.Color(0x66FFFF))
+        embed = discord.Embed(title="GodMode9 dump/build Guide", color=discord.Color(0x66FFFF))
         embed.set_author(name="ih8ih8sn0w", url="https://pastebin.com/sx8HYULr")
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/167592699149418496/9a048f7363e6026307efb29a9cdba4cf.jpg")
         embed.url = "https://pastebin.com/sx8HYULr"
@@ -291,9 +291,9 @@ class Assistance:
     @commands.command()
     async def flashcart(self):
         """Launcher for old flashcarts"""
-        embed = discord.Embed(title="Launcher for old flashcards (r4,m3,dstt,dsx,etc", color=discord.Color(0x42f462))
-        embed.set_author(name="Apache THunder", url="https://gbatemp.net/threads/r4-stage2-twl-flashcart-launcher-and-perhaps-other-cards-soon%E2%84%A2.416434/")
-        embed.set_thumbnail(url="hhttps://gbatemp.net/data/avatars/m/105/105648.jpg")
+        embed = discord.Embed(title="Launcher for old flashcards (r4,m3,dstt,dsx,etc)", color=discord.Color(0x42f462))
+        embed.set_author(name="Apache Thunder", url="https://gbatemp.net/threads/r4-stage2-twl-flashcart-launcher-and-perhaps-other-cards-soon%E2%84%A2.416434/")
+        embed.set_thumbnail(url="https://gbatemp.net/data/avatars/m/105/105648.jpg")
         embed.url = "https://gbatemp.net/threads/r4-stage2-twl-flashcart-launcher-and-perhaps-other-cards-soon%E2%84%A2.416434/"
         embed.description = "Launcher for old flashcards"
         await self.bot.say("", embed=embed)
