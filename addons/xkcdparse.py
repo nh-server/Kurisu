99a36a0d4f3ffe9cb17830685242b15c6693379e
ihaveamac <ian@ianburgwin.net>
2017-05-17 19:34:58 -0700

    disable xkcd link parsing, Discord does this now

--- addons/xkcdparse.py
+++ addons/xkcdparse.py
@@ -54,14 +54,14 @@ class xkcdparse:
         else:
             await self.bot.say("I can't find that one!")
 
-    async def on_message(self, message):
-        # http://stackoverflow.com/questions/839994/extracting-a-url-in-python
-        urls = re.findall(r'(https?://\S+)', message.content)
-        for url in urls:
-            ps = urlparse(url)
-            if ps.netloc == "xkcd.com" or ps.netloc == "www.xkcd.com":
-                comicnum = ps.path.replace('/', '')
-                await self.bot.send_message(message.channel, embed=await self.embed_xkcd_comic(xkcd.getComic(comicnum)))
+    # async def on_message(self, message):
+    #     # http://stackoverflow.com/questions/839994/extracting-a-url-in-python
+    #     urls = re.findall(r'(https?://\S+)', message.content)
+    #     for url in urls:
+    #         ps = urlparse(url)
+    #         if ps.netloc == "xkcd.com" or ps.netloc == "www.xkcd.com":
+    #             comicnum = ps.path.replace('/', '')
+    #             await self.bot.send_message(message.channel, embed=await self.embed_xkcd_comic(xkcd.getComic(comicnum)))
 
 def setup(bot):
     bot.add_cog(xkcdparse(bot))
