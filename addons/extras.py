53cc1141d677614b57252941970f1069444c9b30
notrankin <trevor.roberson19@gmail.com>
2017-11-05 19:24:30 -0500

    Real Turkeybrew

--- addons/extras.py
+++ addons/extras.py
@@ -204,28 +204,28 @@ class Extras:
             await self.bot.say("You don't have a pumpkin!")
 
     @commands.command(pass_context=True, hidden=True)            
-    async def leaf(self, ctx):
+    async def turkey(self, ctx):
         """Turkeybrew"""
         member = ctx.message.author
-        if member.nick and member.nick[-1] == "🍂":
-            await self.bot.say("Your nickname already ends in a leaf!")
+        if member.nick and member.nick[-1] == "🦃":
+            await self.bot.say("Your nickname already ends in a turkey!")
         elif member.name[-1] == "":
-            await self.bot.say("Your name already ends in a leaf!")
+            await self.bot.say("Your name already ends in a turkey!")
         else:
-            await self.bot.change_nickname(member, member.display_name + "🍂")
-            await self.bot.say("Your nickname is now \"{} 🍂\"!".format(member.display_name))
+            await self.bot.change_nickname(member, member.display_name + "🦃")
+            await self.bot.say("Your nickname is now \"{} 🦃\"!".format(member.display_name))
             
     @commands.command(pass_context=True)
-    async def noleaf(self, ctx):
+    async def noturkey(self, ctx):
         """Tired of it."""
         member = ctx.message.author
-        if member.nick and member.nick[-1] == "🍂":
+        if member.nick and member.nick[-1] == "🦃":
             await self.bot.say("Your nickname is now \"{}\"!".format(member.display_name[0:-1].strip()))
             await self.bot.change_nickname(member, member.display_name[0:-1])
-        elif member.name[-1] == "🍂":
-            await self.bot.say("Your username is the one with the leaf!")
+        elif member.name[-1] == "🦃":
+            await self.bot.say("Your username is the one with the turkey!")
         else:
-            await self.bot.say("You don't have a leaf!")            
+            await self.bot.say("You don't have a turkey!")            
 
 def setup(bot):
     bot.add_cog(Extras(bot))
