5f9e41e06ec538666a0e67e63335eeb7d16ea04f
Ian Burgwin <ian@ianburgwin.net>
2017-11-30 21:22:48 -0800

    xmas stuff

--- addons/extras.py
+++ addons/extras.py
@@ -203,18 +203,6 @@ class Extras:
         else:
             await self.bot.say("You don't have a pumpkin!")
 
-    @commands.command(pass_context=True, hidden=True)            
-    async def turkey(self, ctx):
-        """Turkeybrew"""
-        member = ctx.message.author
-        if member.nick and member.nick[-1] == "🦃":
-            await self.bot.say("Your nickname already ends in a turkey!")
-        elif member.name[-1] == "":
-            await self.bot.say("Your name already ends in a turkey!")
-        else:
-            await self.bot.change_nickname(member, member.display_name + " 🦃")
-            await self.bot.say("Your nickname is now \"{} 🦃\"!".format(member.display_name))
-            
     @commands.command(pass_context=True)
     async def noturkey(self, ctx):
         """Tired of it."""
@@ -225,7 +213,32 @@ class Extras:
         elif member.name[-1] == "🦃":
             await self.bot.say("Your username is the one with the turkey!")
         else:
-            await self.bot.say("You don't have a turkey!")            
+            await self.bot.say("You don't have a turkey!")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def xmasthing(self, ctx):
+        """It's xmas time."""
+        member = ctx.message.author
+        if member.nick and member.nick[-1] == "🎄":
+            await self.bot.say("Your nickname already ends in an xmas tree!")
+        elif member.name[-1] == "":
+            await self.bot.say("Your name already ends in an xmas tree!")
+        else:
+            new_name = member.display_name + " 🎄"
+            await self.bot.change_nickname(member, new_name)
+            await self.bot.say("Your nickname is now \"{}\"!".format(new_name))
+
+    @commands.command(pass_context=True)
+    async def noxmasthing(self, ctx):
+        """Tired of it."""
+        member = ctx.message.author
+        if member.nick and member.nick[-1] == "🎄":
+            await self.bot.say("Your nickname is now \"{}\"!".format(member.display_name[0:-1].strip()))
+            await self.bot.change_nickname(member, member.display_name[0:-1])
+        elif member.name[-1] == "🎄":
+            await self.bot.say("Your username is the one with the xmas tree!")
+        else:
+            await self.bot.say("You don't have an xmas tree!")
 
 def setup(bot):
     bot.add_cog(Extras(bot))
