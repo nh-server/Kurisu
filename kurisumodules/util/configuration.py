85e5196e348977a5e7a2ca90a610bd321e6557d0
Ian Burgwin <ian@ianburgwin.net>
2018-01-24 03:44:30 -0800

    fix table and columns for kurisumodules.util.configuration.ConfigurationManager

--- kurisumodules/util/configuration.py
+++ kurisumodules/util/configuration.py
@@ -7,7 +7,7 @@ class ConfigurationManager(DatabaseManager):
 
     def __init__(self, bot: kurisu2.Kurisu2, database_path: str):
         self.log.debug('Initializing %s', type(self).__name__)
-        super().__init__('restrictions', bot, database_path)
-        self._create_tables((('user id', 'integer'), ('restriction', 'text')))
+        super().__init__('configuration', bot, database_path)
+        self._create_tables((('key', 'text'), ('value', 'text')))
 
     # TODO: ConfigurationManager
