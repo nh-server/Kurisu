2ba5e49da4822e2898c6fb4dbdcc54072c6e81c4
Ian Burgwin <ian@ianburgwin.net>
2018-04-02 21:56:03 -0700

    change newlines for ctrwup errcodes

--- kurisumodules/data/nin.py
+++ kurisumodules/data/nin.py
@@ -8,32 +8,37 @@ ctrwup = {
     '002-0102': SYSTEM_BANNED,
     '002-0119': 'System update required (outdated friends-module)',
     '002-0120': 'Title update required (outdated title version)',
-    '002-0121': 'Local friend code SEED has invalid signature.\n\nThis should not happen unless it is modified. The '
-                'only use case for modifying this file is for system unbanning, so you cannot ask how to fix this '
-                'issue here.',
+    '002-0121': 'Local friend code SEED has invalid signature.\n'
+                '\n'
+                'This should not happen unless it is modified. The only use case for modifying this file is for '
+                'system unbanning, so you cannot ask how to fix this issue here.',
     '003-1099': 'Access point could not be found with the given SSID.',
     '003-2001': 'DNS error. If using a custom DNS server, make sure the settings are correct.',
     '005-4800': 'HTTP Status 500 (Internal Error), unknown cause(?). eShop servers might have issues.',
     '005-5602': 'Unable to connect to the eShop. This error is most likely the result of an incorrect region '
-                'setting.\nMake sure your region is correctly set in System Settings. If you encounter this error '
-                'after region-changing your system, make sure you followed all the steps properly.',
-    '005-5964': 'Your Nintendo Network ID has been banned from accessing the eShop.\nIf you think this was '
-                'unwarranted, you will have to contact Nintendo Support to have it reversed.',
+                'setting.\n'
+                'Make sure your region is correctly set in System Settings. If you encounter this error after '
+                'region-changing your system, make sure you followed all the steps properly.',
+    '005-5964': 'Your Nintendo Network ID has been banned from accessing the eShop.\n'
+                'If you think this was unwarranted, you will have to contact Nintendo Support to have it reversed.',
     '005-7550': 'Replace SD card(?). Occurs on Nintendo eShop.',
     '006-0102': 'Unexpected error. Could probably happen trying to play an out-of-region title online?',
     '006-0332': 'Disconnected from the game server.',
-    '006-0502': 'Could not connect to the server.\n\n• Check the [network status page]('
-                'http://support.nintendo.com/networkstatus)\n• Move closer to your wireless router\n• Verify DNS '
-                'settings. If "Auto-Obtain" doesn\'t work, try Google\'s Public DNS (8.8.8.8, 8.8.4.4) and try again.',
+    '006-0502': 'Could not connect to the server.\n'
+                '\n'
+                '• Check the [network status page](http://support.nintendo.com/networkstatus)\n'
+                '• Move closer to your wireless router\n• Verify DNS settings. If "Auto-Obtain" doesn\'t work, try '
+                'Google\'s Public DNS (8.8.8.8, 8.8.4.4) and try again.',
     '006-0612': 'Failed to join the session.',
     '007-0200': 'Could not access SD card.',
     '007-2001': 'Usually the result after region-changing the system. New 3DS cannot fix this issue right now.',
-    '007-2100': 'The connection to the Nintendo eShop timed out.\nThis may be due to an ongoing server maintenance, '
-                'check <https://support.nintendo.com/networkstatus> to make sure the servers are operating normally. '
-                'You may also encounter this error if you have a weak internet connection.',
-    '007-2404': 'An error occurred while attempting to connect to the Nintendo eShop.\nMake sure you are running the '
-                'latest firmware, since this error will appear if you are trying to access the eShop on older '
-                'versions.',
+    '007-2100': 'The connection to the Nintendo eShop timed out.\n'
+                'This may be due to an ongoing server maintenance, check <https://support.nintendo.com/networkstatus> '
+                'to make sure the servers are operating normally. You may also encounter this error if you have a '
+                'weak internet connection.',
+    '007-2404': 'An error occurred while attempting to connect to the Nintendo eShop.\n'
+                'Make sure you are running the latest firmware, since this error will appear if you are trying to '
+                'access the eShop on older versions.',
     '007-2720': 'SSL error?',
     '007-2916': 'HTTP error, server is probably down. Try again later?',
     '007-2913': 'HTTP error, server is probably down. Try again later?',
@@ -44,8 +49,10 @@ ctrwup = {
     '009-2916': 'NIM HTTP error, server is probably down. Try again later?',
     '009-2913': 'NIM HTTP error, server is probably down. Try again later?',
     '009-4079': 'Could not access SD card. General purpose error.',
-    '009-4998': '"Local content is newer."\nThe actual cause of this error is unknown.',
-    '009-6106': '"AM error in NIM."\nProbably a bad ticket.',
+    '009-4998': '"Local content is newer."\n'
+                'The actual cause of this error is unknown.',
+    '009-6106': '"AM error in NIM."\n'
+                'Probably a bad ticket.',
     '009-8401': 'Update data corrupted. Delete and re-install.',
     '011-3021': 'Cannot find title on Nintendo eShop. Probably incorrect region, or never existed.',
     '011-3136': 'Nintendo eShop is currently unavailable. Try again later.',
@@ -55,20 +62,26 @@ ctrwup = {
     '022-2452': 'Occurs when trying to use Nintendo eShop with UNITINFO patches enabled.',
     '022-2501': "Attempting to use a Nintendo Network ID on one system when it is linked on another. This can be the "
                 "result of using System Transfer, then restoring the source system's NAND and attempting to use "
-                "services that require a Nintendo Network ID.\n\nIn a System Transfer, all Nintendo Network ID "
-                "accounts associated with the system are transferred over, whether they are currently linked or not.",
+                "services that require a Nintendo Network ID.\n"
+                "\n"
+                "In a System Transfer, all Nintendo Network ID accounts associated with the system are transferred "
+                "over, whether they are currently linked or not.",
     '022-2511': 'System update required (what causes this? noticed while opening Miiverse, probably not friends '
                 'module)',
     '022-2613': 'Incorrect e-mail or password when trying to link an existing Nintendo Network ID. Make sure there '
-                'are no typos, and the given e-mail is the correct one for the given ID.\nIf you forgot the password, '
-                'reset it at <https://id.nintendo.net/account/forgotten-password>',
+                'are no typos, and the given e-mail is the correct one for the given ID.\n'
+                'If you forgot the password, reset it at <https://id.nintendo.net/account/forgotten-password>',
     '022-2631': 'Nintendo Network ID deleted, or not usable on the current system. If you used System Transfer, '
                 'the Nintendo Network ID will only work on the target system.',
     '022-2633': 'Nintendo Network ID temporarily locked due to too many incorrect password attempts. Try again later.',
     '022-2634': 'Nintendo Network ID is not correctly linked on the system. This can be a result of formatting the '
-                'SysNAND using System Settings to unlink it from the EmuNAND.\n\nSee [this part on 3DS Hacks Guide]'
+                'SysNAND using System Settings to unlink it from the EmuNAND.\n'
+                '\n'
+                'See [this part on 3DS Hacks Guide]'
                 '(https://3ds.hacks.guide/godmode9-usage.html#removing-an-nnid-without-formatting-your-device) on '
-                'removing the NNID without formatting.\n\nTinyFormat is recommended for unlinking in the future.',
+                'removing the NNID without formatting.\n'
+                '\n'
+                'TinyFormat is recommended for unlinking in the future.',
     '022-2812': SYSTEM_BANNED,
     '028-0102': SYSTEM_BANNED,
     '032-1820': 'Browser error that asks whether you want to go on to a potentially dangerous website. Can be '
