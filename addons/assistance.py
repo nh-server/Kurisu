8de3ca34861ac7d8ae137c1177ee948a24a1f0df
Jisagi <Jisagi@users.noreply.github.com>
2018-05-15 10:51:39 +0200

    3DS Injector beta 27 link added to vc

--- addons/assistance.py
+++ addons/assistance.py
@@ -367,7 +367,7 @@ class Assistance:
             embed = discord.Embed(title="Virtual Console Injects for 3DS", color=discord.Color.blue())
             embed.set_author(name="Asdolo", url="https://gbatemp.net/members/asdolo.389539/")
             embed.set_thumbnail(url="https://i.imgur.com/rHa76XM.png")
-            embed.url = "https://gbatemp.net/search/40920047/?q=injector&t=post&o=date&g=1&c[title_only]=1&c[user][0]=389539"
+            embed.url = "https://mega.nz/#!qnAE1YjC!q3FRHgIAVEo4nRI2IfANHJr-r7Sil3YpPYE4w8ZbUPY"
             embed.description = "The recommended way to play old classics on your 3DS"
             await self.bot.say("", embed=embed)
             
