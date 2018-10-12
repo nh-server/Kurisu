a821f5f5f4ad751a66c6ea525ab6ea88f4b7d3bc
Jisagi <Jisagi@users.noreply.github.com>
2018-10-12 21:31:38 +0200

    halloween spooky command added

--- addons/extras.py
+++ addons/extras.py
@@ -164,6 +164,18 @@ class Extras:
             await self.bot.say("You don't have a rainbow!")
 
     @commands.command(pass_context=True)
+    async def spooky(self, ctx):
+        """Spookybrew"""
+        member = ctx.message.author
+        if member.nick and member.nick[-1] == "🎃":
+            await self.bot.say("Your nickname already ends in a pumpkin!")
+        elif member.name[-1] == "":
+            await self.bot.say("Your name already ends in a pumpkin!")
+        else:
+            await self.bot.change_nickname(member, member.display_name + " 🎃")
+            await self.bot.say("Your nickname is now \"{} 🎃\"!".format(member.display_name))  
+
+    @commands.command(pass_context=True)
     async def nospooky(self, ctx):
         """Tired of it."""
         member = ctx.message.author
