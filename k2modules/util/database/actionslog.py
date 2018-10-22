29ad2aeef49a720e58f65935fad0373eab2b54f4
Ian Burgwin <ian@ianburgwin.net>
2018-10-21 20:47:26 -0700

    add "extra" column to actions_log table, set description to None by default in add_entry

--- k2modules/util/database/actionslog.py
+++ k2modules/util/database/actionslog.py
@@ -11,7 +11,7 @@ if TYPE_CHECKING:
     from typing import Generator, Optional
 
 tables = {'actions_log': OrderedDict((('entry_id', 'blob'), ('user_id', 'blob'), ('target_id', 'blob'),
-                                      ('kind', 'text'), ('description', 'text'))),
+                                      ('kind', 'text'), ('description', 'text'), ('extra', 'text'))),
           'attachments': OrderedDict((('entry_id', 'blob'), ('url', 'text')))}
 
 
@@ -21,18 +21,19 @@ class ActionEntry(NamedTuple):
     target_id: int
     date: datetime
     kind: str
-    description: str
+    description: 'Optional[str]'
+    extra: 'Optional[str]'
 
 
 class ActionsLogDatabaseManager(BaseDatabaseManager, tables=tables):
     """Manages the actions_log database."""
 
-    def add_entry(self, user_id: int, target_id: int, kind: str, description: 'Optional[str]',
+    def add_entry(self, user_id: int, target_id: int, kind: str, description: str = None, extra: str = None,
                   entry_id: int = None) -> int:
         """Add an action entry."""
         now = entry_id or time_snowflake(datetime.now())
         self._insert('actions_log', entry_id=i2s(now), user_id=i2s(user_id), target_id=i2s(target_id),
-                     type=kind, description=description)
+                     type=kind, description=description, extra=extra)
         return now
 
     def get_entries(self, *, entry_id: int = None, user_id: int = None, target_id: int = None,
@@ -47,13 +48,14 @@ class ActionsLogDatabaseManager(BaseDatabaseManager, tables=tables):
             values['target_id'] = i2s(target_id)
         if kind:
             values['kind'] = kind
-        for action_id, user_id, target_id, kind, description in self._select('actions_log', **values):
+        for action_id, user_id, target_id, kind, description, extra in self._select('actions_log', **values):
             yield ActionEntry(entry_id=s2i(action_id),
                               user_id=s2i(user_id),
                               target_id=s2i(target_id),
                               date=snowflake_time(s2i(action_id)),
                               kind=kind,
-                              description=description)
+                              description=description,
+                              extra=extra)
 
     def add_attachment(self, entry_id: int, url: str):
         """Add an attachment to an action entry."""
