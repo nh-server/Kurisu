a6b00faab5271fb495eb290a26a336603f8c3e60
Chen Zheng Wei <chenzw95@users.noreply.github.com>
2018-10-31 09:02:47 +0800

    ImageConvert: use non-blocking aiohttp instead

--- addons/imgconvert.py
+++ addons/imgconvert.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-import requests
+import aiohttp
 from PIL import Image
 
 class ImageConvert:
@@ -14,14 +14,16 @@ class ImageConvert:
         # BMP conversion
         for f in message.attachments:
             if f["filename"].lower().endswith('.bmp') and f["size"] <= 600000:  # 600kb
-                img_request = requests.get(f["url"])
-                img_obj = Image.open(BytesIO(img_request.content))
-                img_out = BytesIO()
-                img_obj.save(img_out, 'png')
-                img_out.seek(0)
-                out_message = "{} from {}".format(self.bot.escape_name(f["filename"]), message.author.mention)
-                new_filename = f["filename"][:-3] + "png"
-                await self.bot.send_file(message.channel, img_out, filename=new_filename, content=out_message)
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f["url"]) as img_request:
+                        img_content = await img_request.read()
+                        img_obj = Image.open(BytesIO(img_content))
+                        img_out = BytesIO()
+                        img_obj.save(img_out, 'png')
+                        img_out.seek(0)
+                        out_message = "{} from {}".format(self.bot.escape_name(f["filename"]), message.author.mention)
+                        new_filename = f["filename"][:-3] + "png"
+                        await self.bot.send_file(message.channel, img_out, filename=new_filename, content=out_message)
 
 def setup(bot):
     bot.add_cog(ImageConvert(bot))
