28783d0a06f38b106e3f0404ee6fe08c3bfad9fc
Chen Zheng Wei <chenzw95@users.noreply.github.com>
2018-11-01 13:30:30 +0800

    ImageConvert: offload conversion to subprocess

--- addons/imgconvert.py
+++ addons/imgconvert.py
@@ -1,5 +1,7 @@
 from io import BytesIO
 import aiohttp
+import concurrent.futures
+import functools
 from PIL import Image
 
 class ImageConvert:
@@ -10,17 +12,22 @@ class ImageConvert:
         self.bot = bot
         print('Addon "{}" loaded'.format(self.__class__.__name__))
 
+    def img_convert(self, in_img):
+        img_obj = Image.open(BytesIO(in_img))
+        img_out = BytesIO()
+        img_obj.save(img_out, 'png')
+        img_out.seek(0)
+        return img_out
+
     async def on_message(self, message):
         # BMP conversion
         for f in message.attachments:
             if f["filename"].lower().endswith('.bmp') and f["size"] <= 600000:  # 600kb
                 async with aiohttp.ClientSession() as session:
-                    async with session.get(f["url"]) as img_request:
+                    async with session.get(f["url"], timeout=45) as img_request:
                         img_content = await img_request.read()
-                        img_obj = Image.open(BytesIO(img_content))
-                        img_out = BytesIO()
-                        img_obj.save(img_out, 'png')
-                        img_out.seek(0)
+                        with concurrent.futures.ProcessPoolExecutor() as pool:
+                            img_out = await self.bot.loop.run_in_executor(pool, functools.partial(img_convert, img_content))
                         out_message = "{} from {}".format(self.bot.escape_name(f["filename"]), message.author.mention)
                         new_filename = f["filename"][:-3] + "png"
                         await self.bot.send_file(message.channel, img_out, filename=new_filename, content=out_message)
