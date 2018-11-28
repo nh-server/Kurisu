4d49960f0c39141ac379857dc8d559a3b5c3e491
eip618 <36315290+eip618@users.noreply.github.com>
2018-11-28 19:13:08 +1100

    fix for lumabug random \
    adds a space in order to make a new line function correctly

--- addons/assistance.py
+++ addons/assistance.py
@@ -353,7 +353,7 @@ follow these steps:
                                 3. Power on the console.
                                 4. Leave the console open and powered on for 10-15 minutes. Do not touch the console \
 during this time.
-                                If the console boots successfully in that time, the bug is now fixed and is unlikely to\ 
+                                If the console boots successfully in that time, the bug is now fixed and is unlikely to \ 
 happen again. If the console still fails to boot to home menu, come back and ask for more help. Mention that you have \
 already tried the 'Luma black screen process.
                                 """, title="Luma Black Screen Bug")
