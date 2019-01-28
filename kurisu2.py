bfd2c412fecca9b11257ec5a5263dcc6e83d17e8
Ian Burgwin <ian@ianburgwin.net>
2019-01-27 16:36:33 -0800

    set required Python version to 3.7.2

--- kurisu2.py
+++ kurisu2.py
@@ -273,8 +273,8 @@ def main(*, debug=False, change_directory=False):
         print(f'discord.py is not at least 1.0.0x. (current version: {discord.__version__})')
         return 2
 
-    if not hexversion >= 0x030701F0:  # 3.7.1
-        print('Kurisu2 requires 3.7.1 or later.')
+    if not hexversion >= 0x030702F0:  # 3.7.2
+        print('Kurisu2 requires 3.7.2 or later.')
         return 2
 
     if change_directory:
