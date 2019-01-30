c9990ea6a27d11b670114ba8419e19ea78c29848
Ian Burgwin <ian@ianburgwin.net>
2019-01-29 20:35:52 -0800

    update netinfo initial embed message

--- addons/loop.py
+++ addons/loop.py
@@ -41,7 +41,7 @@ class Loop:
 
     tz = pytz.timezone('US/Pacific')
 
-    netinfo_embed = discord.Embed(description="This needs to be set up")
+    netinfo_embed = discord.Embed(description="The Network Maintenance Information page has not been successfully checked yet.")
     # netinfo_future_embed = discord.Embed(description="This needs to be set up")
 
     def netinfo_parse_time(self, timestr):
