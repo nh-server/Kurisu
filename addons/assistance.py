5886499f8976feef40cab94af733249d7501cc52
eip618 <36315290+eip618@users.noreply.github.com>
2019-02-24 17:50:19 +1100

    remove line identifiers
    i was unaware \n was no longer required

--- addons/assistance.py
+++ addons/assistance.py
@@ -293,10 +293,10 @@ NAND backups, and SD card contents. Windows, macOS, and Linux are supported.
     async def hbl(self):
         """Get Homebrew Launcher working on 11.4+ firmware"""
         await self.simple_embed("""
-                                If you wish to access the Homebrew Launcher on 11.4+, you have two options.\n
-                                First of all, you can use Steelminer, a free exploit to install the Homebrew Launcher. However, homebrew-only access has disadvantages.\n
-                                For example, homebrew-only is often unstable and crashes for no reason. Also, it is limited in features and system access.\n
-                                The second option is to install CFW, or custom firmware. Please use `.guide 3ds` for a list of ways to get CFW.\n
+                                If you wish to access the Homebrew Launcher on 11.4+, you have two options.
+                                First of all, you can use Steelminer, a free exploit to install the Homebrew Launcher. However, homebrew-only access has disadvantages.
+                                For example, homebrew-only is often unstable and crashes for no reason. Also, it is limited in features and system access.
+                                The second option is to install CFW, or custom firmware. Please use `.guide 3ds` for a list of ways to get CFW.
                                 Here is a [Steelhax guide](https://git.io/fhbGY). Do NOT proceed to `Installing boot9strap` if you do not want CFW.
                                 """)
 
