d1cb50e432f12016038a2c149c3a22bc0efe964a
TheGinGear <39550332+TheGinGear@users.noreply.github.com>
2019-03-06 13:32:04 -0600

    Fixing Switch Bans Command
    Previous edits were probably auto-formatted to include non-compatible line matching or whatecer. Buncha spaces deleted

--- addons/assistance.py
+++ addons/assistance.py
@@ -720,14 +720,16 @@ You would need to inject Hekate and select `Power off` from the menu to fully tu
     async def nxban(self):
         """Switch ban risk snippet"""
         await self.simple_embed("""
-                                The Switch is a much more secure system than the 3DS, and Nintendo has upped their game when it comes to bans. 
-                                One of the main reasons for this is that there are significantly more monitoring systems, \
-                                some of which cannot be turned off.
-                                Remember that you can only reduce your chances of getting banned; nothing is guaranteed and you could be banned \
-                                at any time if you decide to hack your device. It will always be a cat and mouse game, until or unless there are big changes \
-                                in the scene. \
-                                Refer to #faq-switch for a list of things to avoid doing to reduce your risks.
-                                You cannot ask about unbanning your console here.
+                                The Switch is a much more secure system than the 3DS, and Nintendo has upped their game when it comes to bans. \
+One of the main reasons for this is that there are significantly more monitoring systems, \
+some of which cannot be turned off.
+
+Remember that you can only reduce your chances of getting banned; nothing is guaranteed and you could be banned \
+ at any time if you decide to hack your device. It will always be a cat and mouse game, until or unless there are big changes \
+in the scene. 
+ 
+Refer to #faq-switch for a list of things to avoid doing to reduce your risks.
+You cannot ask about unbanning your console here.
                                 """, title="Switch Bans")
 
 def setup(bot):
