0d38cc2cd3c88b12e7244e40de95633a6211cebf
TheGinGear <39550332+TheGinGear@users.noreply.github.com>
2019-03-06 15:22:41 -0600

    Update assistance.py

--- addons/assistance.py
+++ addons/assistance.py
@@ -694,6 +694,7 @@ You would need to inject Hekate and select `Power off` from the menu to fully tu
                                 
                                 • You will depend on your SD Card to take advantage of CFW. If your microSD is unavailable, you can only boot into stock firmware. \
 `**Warning: if performed a fuseless update from <4.1.0, booting stock __will__ update your game card slot, rendering it unusable when you run <4.1.0!`
+
                                 • The console charges slowly in RCM. To solve this, boot Hekate, then inject Atmosphère.\
 If you see the battery icon on the top left of the screen, then the console is charging at its normal speed.
                                 To use AutoRCM, boot `Hekate` > `Tools` > `AutoRCM`. To uninstall, go to the same location.
@@ -722,9 +723,12 @@ If you see the battery icon on the top left of the screen, then the console is c
                                 The Switch is a much more secure system than the 3DS, and Nintendo has upped their game when it comes to bans. 
                                 One of the main reasons for this is that there are significantly more monitoring systems, \
 some of which cannot be turned off.
+
                                 Remember that you can only reduce your chances of getting banned; nothing is guaranteed and you could be banned \
 at any time if you decide to hack your device. It will always be a cat and mouse game, until or unless there are big changes \
-in the scene. Refer to #faq-switch for a list of things to avoid doing to reduce your risks.
+in the scene. 
+
+                                Refer to <#465640445913858048> for a list of things to avoid doing to reduce your risks.
                                 You cannot ask about unbanning your console here.
                                 """, title="Switch Bans")
 
