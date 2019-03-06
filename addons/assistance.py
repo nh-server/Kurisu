01e6a6fa3dd8ddad22cd07a622e05ef8c6aab4a9
FrozenChen <brobles@umag.cl>
2019-03-06 17:09:01 -0300

    Fix nxban text

--- addons/assistance.py
+++ addons/assistance.py
@@ -722,11 +722,10 @@ You would need to inject Hekate and select `Power off` from the menu to fully tu
         await self.simple_embed("""
                                 The Switch is a much more secure system than the 3DS, and Nintendo has upped their game when it comes to bans. 
                                 One of the main reasons for this is that there are significantly more monitoring systems, \
-                                some of which cannot be turned off.
+some of which cannot be turned off.
                                 Remember that you can only reduce your chances of getting banned; nothing is guaranteed and you could be banned \
-                                at any time if you decide to hack your device. It will always be a cat and mouse game, until or unless there are big changes \
-                                in the scene. \
-                                Refer to #faq-switch for a list of things to avoid doing to reduce your risks.
+at any time if you decide to hack your device. It will always be a cat and mouse game, until or unless there are big changes \
+in the scene. Refer to #faq-switch for a list of things to avoid doing to reduce your risks.
                                 You cannot ask about unbanning your console here.
                                 """, title="Switch Bans")
 
