9f0ec9ea39c6a813ceed1bb085a0f70f46b94b73
eip618 <36315290+eip618@users.noreply.github.com>
2019-03-06 14:36:50 +1100

    eip and the whitespace saga (6)

--- addons/assistance.py
+++ addons/assistance.py
@@ -699,18 +699,18 @@ your device will refuse to write to it.
     @commands.cooldown(rate=1, per=30.0, type=commands.BucketType.channel)
     async def nxban(self, ctx, *, console=""):
         """Switch ban risk snippet"""
-            if self.check_console(console, ctx.message.channel.name, ('switch', 'nx')):
-                await self.simple_embed("""
-                                        The Switch is a much more secure system than the 3DS, and Nintendo has upped their game when it comes to bans. 
-                                        One of the main reasons for this is that there are significantly more monitoring systems, \
-                                        some of which cannot be turned off.
-                                        Remember that you can only reduce your chances of getting banned; nothing is guaranteed and you could be banned \
-                                        at any time.
-                                        if you decide to hack your device. It will always be a cat and mouse game, until or unless there are big changes \
-                                        in the scene.
-                                        Refer to #faq-switch for a list of things to avoid doing to reduce your risks.
-                                        You cannot ask about unbanning your console here.
-                                        """, title="Switch Bans")
+        if self.check_console(console, ctx.message.channel.name, ('switch', 'nx')):
+            await self.simple_embed("""
+                                    The Switch is a much more secure system than the 3DS, and Nintendo has upped their game when it comes to bans. 
+                                    One of the main reasons for this is that there are significantly more monitoring systems, \
+                                    some of which cannot be turned off.
+                                    Remember that you can only reduce your chances of getting banned; nothing is guaranteed and you could be banned \
+                                    at any time.
+                                    if you decide to hack your device. It will always be a cat and mouse game, until or unless there are big changes \
+                                    in the scene.
+                                    Refer to #faq-switch for a list of things to avoid doing to reduce your risks.
+                                    You cannot ask about unbanning your console here.
+                                    """, title="Switch Bans")
 
 def setup(bot):
     bot.add_cog(Assistance(bot))
