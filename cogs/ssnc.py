55f45ae830186439f4b2cf2b34af1bf7c4914c1b
noirscape <deepnavy@waifu.club>
2019-03-29 18:04:06 +0100

    Fixed logic in Serial Checker
    The original was made with an incorrect JSON file in mind. Now uses the GBATemp thread we also use in our guide to verify the serials.

--- cogs/ssnc.py
+++ cogs/ssnc.py
@@ -1,3 +1,30 @@
+# kirigiri - A discord bot.
+# Copyright (C) 2018 - Valentijn "noirscape" V.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation at version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# In addition, the additional clauses 7b and 7c are in effect for this program.
+#
+# b) Requiring preservation of specified reasonable legal notices or
+# author attributions in that material or in the Appropriate Legal
+# Notices displayed by works containing it; or
+#
+# c) Prohibiting misrepresentation of the origin of that material, or
+# requiring that modified versions of such material be marked in
+# reasonable ways as different from the original version; or
+
+# Also licensed under the ISC license.
+import discord
 from discord.ext import commands
 from discord.ext.commands import Cog
 import re
@@ -26,49 +53,61 @@ class SwitchSerialNumberCheck(Cog):
 
         assembly_line = int(serial[3])
         checking_value = int(serial[3:10])
-        print(f"{checking_value} : {len(str(checking_value))}")
         safe_serial = serial[:9] + 'XXXX'
 
         if region == 'J':
             if assembly_line == 1:
-                if 1002100 <= checking_value <= 1002999:
+                if checking_value < 1002000:
+                    pass
+                elif 1002000 <= checking_value < 1003000:
                     maybe = True
                 elif checking_value >= 1003000:
-                    pass
+                    patched = True
 
             elif assembly_line == 4:
-                if 4004700 <= checking_value <= 4005999:
+                if checking_value < 4004600:
+                    pass
+                elif 4004600 <= checking_value < 4006000:
                     maybe = True
                 elif checking_value >= 4006000:
-                    pass
+                    patched = True
 
             elif assembly_line == 7:
-                if 7004100 <= checking_value <= 7004999:
+                if checking_value < 7004000:
+                    pass
+                elif 7004000 <= checking_value < 7005000:
                     maybe = True
                 elif checking_value >= 7005000:
-                    pass
-
-            elif assembly_line == 9:
-                maybe = True
+                    patched = True
 
         elif region == 'W':
             if assembly_line == 1:
-                if 1007900 <= checking_value <= 1008199:
-                    maybe = True
-                elif checking_value >= 1008200:
+                if checking_value < 1007400:
                     pass
+                elif 1007400 <= checking_value < 1012000:  # GBATemp thread is oddly disjointed here, proper value could
+                    # be 1007500, not sure.
+                    maybe = True
+                elif checking_value >= 1012000:
+                    patched = True
 
             elif assembly_line == 4:
-                if 4001200 <= checking_value <= 4002999:
-                    maybe = True
-                elif checking_value >= 4003000:
+                if checking_value < 4001100:
                     pass
+                elif 4001100 <= checking_value < 4001200:
+                    maybe = True
+                elif checking_value >= 4001200:
+                    patched = True
 
             elif assembly_line == 7:
-                if 7001790 <= checking_value <= 7002999:
+                if checking_value < 7001780:
+                    pass
+                elif 7001780 <= checking_value < 7003000:
                     maybe = True
                 elif checking_value >= 7003000:
-                    pass
+                    patched = True
+
+            elif assembly_line == 9:
+                maybe = True
 
         elif region == 'K':
             maybe = True
