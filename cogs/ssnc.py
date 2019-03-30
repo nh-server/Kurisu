9eeabd0d592b66fbd0c74e0346e6cb0101658aac
Valentijn <deepnavy@waifu.club>
2019-03-30 01:58:40 +0100

    Remove license header

--- cogs/ssnc.py
+++ cogs/ssnc.py
@@ -1,29 +1,3 @@
-# kirigiri - A discord bot.
-# Copyright (C) 2018 - Valentijn "noirscape" V.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation at version 3 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-# In addition, the additional clauses 7b and 7c are in effect for this program.
-#
-# b) Requiring preservation of specified reasonable legal notices or
-# author attributions in that material or in the Appropriate Legal
-# Notices displayed by works containing it; or
-#
-# c) Prohibiting misrepresentation of the origin of that material, or
-# requiring that modified versions of such material be marked in
-# reasonable ways as different from the original version; or
-
-# Also licensed under the ISC license.
 from discord.ext import commands
 from discord.ext.commands import Cog
 import re
