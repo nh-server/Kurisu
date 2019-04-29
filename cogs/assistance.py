468dd01a6bf7aab1256f49ddd2e9fd4970705cd7
eip618 <36315290+eip618@users.noreply.github.com>
2019-04-29 12:13:50 +1000

    add spacing changes
    stability updates :tm:

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -443,11 +443,13 @@ just missing a file called boot.firm in the root of your SD card.
     async def bfm4(self, ctx):
         """Information about BruteforceMovable and how the friend code may not always be needed"""
         await self.simple_embed(ctx, """
-                                If BruteforceMovable is now at step 4, download your movable.sed and continue.\
-You do not need to do anything more related to movable_part1.sed, Python, or the\
+                                If BruteforceMovable is now at step 4, download your movable.sed and continue. \
+You do not need to do anything more related to movable_part1.sed, Python, or the \
 command line. movable.sed is the final product and requires no further processing.
-                                **You do not need to go back and get the friend code, or do anything more with the friend code.**
-                                **It does not matter if the friend does not add you back.**
+                                **You do not need to go back and get the friend code, or do anything more 
+                                with the friend code.
+                                It does not matter if the friend does not add you back.
+                                The bot already has your information and has removed you as a friend.**
                                 """, title="BruteforceMovable Advice")
 
     @commands.command()
