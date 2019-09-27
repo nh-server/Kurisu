68a5c4d9b21a839911e3d9928db2334c656451b5
Glazed_Belmont <48196637+GlaZedBelmont@users.noreply.github.com>
2019-09-27 12:00:16 -0400

    added a cIOS installation guide command

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -916,6 +916,16 @@ your device will refuse to write to it.
         embed.description = "An AP-Patching guide"
         await ctx.send(embed=embed)
                              
+   @commands.command
+    async def cios(self, ctx):
+        """cIOS installation guide"""
+        embed = discord.Embed(title="cIOS Guide", color=discord.Color.green())
+        embed.set_author(name="tj_cool")
+        embed.set_thumbnail(url="https://i.imgur.com/sXSNYyV.jpg")
+        embed.url = "https://sites.google.com/site/completesg/backup-launchers/installation
+        embed.description = "A cIOS installation guide"
+        await ctx.send(embed=embed)
+                             
     @commands.command()
     async def sdroot(self, ctx):
         """Picture to say what the heck is the root""" 
