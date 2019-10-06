215bd3b0124f9acc152ef64c2cbdf5828073e57a
pbanj <pbanj@users.noreply.github.com>
2019-10-06 18:21:46 -0400

    Ridley can't spell

--- cogs/memes.py
+++ cogs/memes.py
@@ -382,7 +382,7 @@ class Memes(commands.Cog):
 
     @commands.command(hidden=True)
     @commands.cooldown(rate=1, per=5.0, type=commands.BucketType.channel)
-    async def gooseanna(self, ctx):
+    async def bananoose(self, ctx):
         """:)"""
         await self._meme(ctx, "https://i.imgur.com/VUmkXDd.jpg")
 
