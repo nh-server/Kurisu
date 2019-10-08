f10bdcde4aaaa261f0cdff8e6d2f605d0d192ab7
The Dax <thedax@users.noreply.github.com>
2019-10-08 01:31:50 -0400

    Old patched: 8.0.0 and higher, not 8.1.0.

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -351,7 +351,7 @@ and helpers can be found in #welcome-and-rules if you don't know who they are.
                 All of these can run [Atmosphere](https://nh-server.github.io/switch-guide/). Make sure that Atmosphere is compatible with the latest firmware version before you update.
 
                 **Second generation consoles ("patched" units, Switch Lite, Mariko, etc.)**
-                "Old" Patched Switch (HAC-001): Do NOT update past 7.0.1. Units on 7.0.1 and below will eventually get CFW. Units on 8.1.0 and higher are not expected to be hacked and can be updated.
+                "Old" Patched Switch (HAC-001): Do NOT update past 7.0.1. Units on 7.0.1 and below will eventually get CFW. Units on 8.0.0 and higher are not expected to be hacked and can be updated.
                 "New" Switch (HAC-001-01): Do NOT update past 8.0.1. Units on 8.0.1 and below will likely get homebrew. Units on 8.1.0 and higher are not expected to be hacked and can be updated.
                 Switch Lite (HDH-001): Do NOT update past 8.0.1. Units on 8.0.1 and below will likely get homebrew. Units on 8.1.0 and higher are not expected to be hacked and can be updated.
 
