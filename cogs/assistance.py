09caad0e9663d22f6998b285378e9e83c298c553
win9x-se <51892981+win9x-se@users.noreply.github.com>
2019-10-26 20:48:36 -0400

    fixed a remanet of lumacheck in nospace (#636)

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -206,7 +206,7 @@ versions on 11.8+ will cause a blackscreen until you update.
     async def nospace(self, ctx):
         """Low space NAND Backup"""
         embed = discord.Embed(title="How to create a 3DS NAND backup without enough space on the SD card", color=discord.Color.blue())
-        embed.add_field(name="Steps to open Luma Configuration", value=cleandoc("""
+        embed.add_field(name="Steps to create the backup", value=cleandoc("""
                 1. Copy the Nintendo 3DS folder from the root of your SD card to your computer then delete it from **the SD card.**
                 2. Boot GodMode9 by holding START on boot then preform a normal NAND backup. After that, power off the system.
                 3. Copy the files in gm9/out on your SD card to a safe spot on your computer. Then, delete the files from **the SD card.**
