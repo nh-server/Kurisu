d6348768fe9af14b98e63471f4d9d5aed48daa47
Halvorsen256 <22641804+Halvorsen256@users.noreply.github.com>
2019-12-05 05:00:23 -0500

    clarified commands for nxupdate

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -958,14 +958,14 @@ as custom themes must be reinstalled for most firmware updates.
                                 When an update is downloaded, but not installed, the console will not display the \
 firmware version in System Settings.
                                 
-                                • To reset on sysNAND,  *power the console off* (hold the power button, follow on-screen prompts).\
+                                • To reset on a stock system,  *power the console off* (hold the power button, follow on-screen prompts).\
 __Hold__ Volume - and Volume +, then Power. When you see Maintenance Mode, you \
 can reboot, and check System Settings.
-                                • On emuMMC, launch Hekate, select your emuMMC boot option, then immediately\
+                                • If using CFW, launch Hekate, select your boot option, then immediately\
 __Hold__ Volume - and Volume +. When you see Maintenance Mode, you \
-can reboot emuMMC, and check System Settings.
+can reboot, and check System Settings.
                                 
-                                *To block automatic update downloads, type .90dns in this chat for further information.*
+                                *To block automatic update downloads, type '.90dns' here for further information.*
                                  """, title="How to delete pending Switch Updates")
 
     @commands.command()
