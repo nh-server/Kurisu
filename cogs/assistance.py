89254f47e8a37b57717ac87185c73874d0190fd7
Halvorsen256 <22641804+Halvorsen256@users.noreply.github.com>
2019-12-06 13:18:39 -0500

    Added emuMMC instructions (#658)

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -958,11 +958,14 @@ as custom themes must be reinstalled for most firmware updates.
                                 When an update is downloaded, but not installed, the console will not display the \
 firmware version in System Settings.
                                 
-                                • To fix this, *power the console off* (hold the power button, follow on-screen prompts).\
-***Hold*** Volume Down and Volume Up, then Power. When you see Maintenance Mode, you \
-may reboot, and check System Settings.
+                                • To reset on a stock system,  *power the console off* (hold the power button, follow on-screen prompts).\
+__Hold__ Volume - and Volume +, then Power. When you see Maintenance Mode, you \
+can reboot, and check System Settings.
+                                • If you're using CFW, launch Hekate, select your boot option, then immediately\
+__Hold__ Volume - and Volume +. When you see Maintenance Mode, you \
+can reboot, and check System Settings.
                                 
-                                *To block automatic update downloads, type .90dns in this chat for further information.*
+                                *To block automatic update downloads, type '.90dns' here for further information.*
                                  """, title="How to delete pending Switch Updates")
 
     @commands.command()
