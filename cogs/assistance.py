49db5ce91a4e9a38f9faba334fce8df751e8c686
The Dax <thedax@users.noreply.github.com>
2019-12-08 19:52:33 -0500

    Fix an additional typo

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -947,7 +947,7 @@ how to delete it.*
                                      *To find Hekate's version information, once Hekate starts, look in the top left corner of the screen. If you use auto-boot, hold `volume -` to stop it.*
                                      
                                      **If you use a custom theme (Atmosphere 0.10.0 and above)**
-                                     Delete or rename `/atmosphere/content/0100000000001000` on your SD card prior to updating, \
+                                     Delete or rename `/atmosphere/contents/0100000000001000` on your SD card prior to updating, \
 as custom themes must be reinstalled for most firmware updates. **Note: On Atmosphere 0.9.4 or below, `contents` is called `titles`.**
                                 """), title="What do I need to do before updating my system firmware when running CFW?")
 
