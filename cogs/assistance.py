387570bb7a2948b95b3eb280052b8980ea8ee7a5
Glazed_Belmont <48196637+GlaZedBelmont@users.noreply.github.com>
2019-12-08 20:58:26 -0500

    quality of life changes for .delupdate (#659)
    * quality of life changes for .delupdate
    
    * edit :D

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -961,13 +961,13 @@ as custom themes must be reinstalled for most firmware updates. **Note: On Atmos
 firmware version in System Settings.
                                 
                                 • To reset on a stock system,  *power the console off* (hold the power button, follow on-screen prompts).\
-__Hold__ Volume - and Volume +, then Power. When you see Maintenance Mode, you \
+ __Hold__ Volume - and Volume +, then Power. When you see Maintenance Mode, you \
 can reboot, and check System Settings.
                                 • If you're using CFW, launch Hekate, select your boot option, then immediately\
-__Hold__ Volume - and Volume +. When you see Maintenance Mode, you \
+ __Hold__ Volume - and Volume +. When you see Maintenance Mode, you \
 can reboot, and check System Settings.
                                 
-                                *To block automatic update downloads, type '.90dns' here for further information.*
+                                To block automatic update downloads, type '.90dns' in <#261581918653513729> for further information.
                                  """, title="How to delete pending Switch Updates")
 
     @commands.command()
