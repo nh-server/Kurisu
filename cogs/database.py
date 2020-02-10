7012c6b0c4e94b0b58d53b8ac06e7f82b893d407
Ian Burgwin <ian@ianburgwin.net>
2020-02-10 09:01:00 -0800

    Revert "cogs.database: add get_time_restrictions_by_user"
    This reverts commit dcee3d52979caf44afb60e336067fc84e3fee123.

--- cogs/database.py
+++ cogs/database.py
@@ -159,11 +159,6 @@ class DatabaseCog(commands.Cog):
         async with self.bot.holder as cur:
             await cur.execute('DELETE FROM timed_restrictions WHERE user_id=? AND type=?', (user_id, type))
 
-    async def get_time_restrictions_by_user(self, userid, type):
-        async with self.bot.holder as cur:
-            await cur.execute('SELECT * from timed_restrictions WHERE user_id=?', (userid,))
-            return await cur.fetchone()
-
     async def get_time_restrictions_by_user_type(self, userid, type):
         async with self.bot.holder as cur:
             await cur.execute('SELECT * from timed_restrictions WHERE user_id=? AND type=?', (userid,type))
