63ef1a56524d363cbf4a1df60b538feda93c1ac6
FrozenChen <brobles@umag.cl>
2020-02-16 14:09:37 -0400

    remove extra whitespace

--- cogs/memes.py
+++ cogs/memes.py
@@ -419,7 +419,7 @@ class Memes(commands.Cog):
     @commands.command(hidden=True)
     @commands.cooldown(rate=5, per=30.0, type=commands.BucketType.channel)
     async def nobrain(self, ctx, action="hacc"):
-        """h a c c"""        
+        """h a c c"""    
         await self._meme(ctx, f'`I have no brain and I must {" ".join(action)}`')
 
     @commands.command(hidden=True, aliases=["wheresource", "sauce", "github"])
