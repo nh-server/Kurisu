2533b9cb0357bd471f432c467ddf5fc256d72dd5
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2020-05-02 11:17:22 +0100

    tweak wording

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -1230,7 +1230,7 @@ in the scene.
                                 - `207.246.121.77` (USA)
                                 - `163.172.141.219`(France)
 
-                                To set these [follow these steps](https://nh-server.github.io/switch-guide/extras/blocking_updates/)
+                                [Follow these steps](https://nh-server.github.io/switch-guide/extras/blocking_updates/) to set up 90dns and ensure it isn't being blocked
 
                                 You will have to manually set these for each WiFi connection you have set up.""",
                                 title="90DNS IP adressses")
