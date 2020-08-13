e501da473f6c2962c9e76d5d0d8325bb5c2f4353
romhacks <kabob8933@gmail.com>
2020-08-13 17:52:41 -0400

    Change text from .hbl to be more recent

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -529,11 +529,10 @@ and helpers can be found in #welcome-and-rules if you don't know who they are.
     async def hbl(self, ctx):
         """Get Homebrew Launcher working on 11.4+ firmware"""
         await self.simple_embed(ctx, """
-                                If you wish to access the Homebrew Launcher on 11.4+, you have two options.
-                                First of all, you can use Steelminer, a free exploit to install the Homebrew Launcher. However, homebrew-only access has disadvantages.
-                                For example, homebrew-only is often unstable and will crash unexpectedly. Also, it is limited in features and system access.
-                                The second option is to install CFW, or custom firmware. Please use `.guide 3ds` for a list of ways to get CFW.
-                                Here is a [Steelhax guide](https://git.io/fhbGY). Do NOT proceed to `Installing boot9strap` if you do not want CFW.
+                                It is now recommended to use a CFW method like unSAFE_MODE
+                                to get Homebrew Launcher access, as these methods are simple and have a wider
+                                compatability with existing homebrew apps than HBL-only exploits.
+                                Use the ".guide 3ds" command to get a link to the recommended guide.
                                 """)
 
     @commands.command()
