e0273903895a0dd6fbaf1137442a2c50bc9b597a
Gunther0042 <57876849+Gunther0042@users.noreply.github.com>
2020-08-16 14:35:09 +0300

    Change .baninfo nx icon
    Changed my icon in the .baninfo nx embed to the one from my Github profile

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -524,7 +524,7 @@ and helpers can be found in #welcome-and-rules if you don't know who they are.
 
         elif self.check_console(console, ctx.message.channel.name, ('nx', 'switch', 'ns')):
             embed = discord.Embed(title="NX Bans", color=discord.Color.purple())
-            embed.set_thumbnail(url="https://eiphax.tech/assets/gunther.png")
+            embed.set_thumbnail(url="https://avatars1.githubusercontent.com/u/57876849?s=400&v=4")
             embed.url = "https://nx.eiphax.tech/ban"
             embed.description = "Bans on the Switch are complicated. Please click the embed header link and read the linked page to learn more."
             await ctx.send(embed=embed)
