8e65e20cd2d833f725bbfaba7a1d793030746745
Glazed_Belmont <48196637+GlaZedBelmont@users.noreply.github.com>
2020-08-31 14:46:11 -0400

    Make Kurisu tell the length for timed restrictions so different timezones don't conflict (#806)

--- cogs/mod.py
+++ cogs/mod.py
@@ -287,7 +287,7 @@ class Mod(DatabaseCog):
         signature = utils.command_signature(ctx.command)
         if not old_timestamp:
             await ctx.send(f"{member.mention} can no longer speak.")
-            msg = f"🔇 **Timed mute**: {issuer.mention} muted {member.mention}| {self.bot.escape_text(member)} until {unmute_time_string} "
+            msg = f"🔇 **Timed mute**: {issuer.mention} muted {member.mention}| {self.bot.escape_text(member)} for {delta}, until {unmute_time_string} "
         else:
             await ctx.send(f"{member.mention} mute was updated.")
             msg = f"🔇 **Timed mute**: {issuer.mention} updated {member.mention}| {self.bot.escape_text(member)} time mute from {old_timestamp} until {unmute_time_string}"
@@ -499,7 +499,7 @@ class Mod(DatabaseCog):
         await utils.send_dm_message(member, msg_user)
         await ctx.send(f"{member.mention} can no longer speak in Assistance Channels.")
         signature = utils.command_signature(ctx.command)
-        msg = f"🚫 **Timed No-Help**: {issuer.mention} restricted {member.mention} until {unnohelp_time_string} | {self.bot.escape_text(member)}"
+        msg = f"🚫 **Timed No-Help**: {issuer.mention} restricted {member.mention} for {delta}, until {unnohelp_time_string} | {self.bot.escape_text(member)}"
         if reason != "":
             msg += "\n✏️ __Reason__: " + reason
         else:
