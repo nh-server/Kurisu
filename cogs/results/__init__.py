15a9063aa4940c4ce690a3eb8a767a85e191ea0e
The Dax <thedax@users.noreply.github.com>
2020-09-21 15:45:19 -0400

    Add results cog It unifies `nxerr` and `err` together with a cleaner codebase. Its supporting code properly parses errors whenever possible and is mostly complete at this point. There are still some missing codes from all consoles, but I plan on adding more as time goes on.

---
+++ cogs/results/__init__.py
@@ -0,0 +1,71 @@
+import discord
+from discord.ext import commands
+
+from . import (nx, cafe, ctr, types)
+
+class Results(commands.Cog):
+    """
+    Parses game console result and error codes.
+    """
+    def __init__(self, bot):
+        self.bot = bot
+
+    def fetch(self, error):
+        if nx.is_valid(error):
+            return nx.get(error)
+        elif ctr.is_valid(error):
+            return ctr.get(error)
+        elif cafe.is_valid(error):
+            return cafe.get(error)
+
+        return None, None, None, types.WARNING_COLOR
+
+    def fixup_input(self, input):
+        # Truncate input to 16 chars so as not to create a huge embed or do
+        # eventual regex on a huge string. If we add support for consoles that
+        # that have longer error codes, adjust accordingly.
+        input = input[:16]
+
+        # Fix up hex input if 0x was omitted
+        # It's fine if it doesn't convert.
+        if not input.startswith('0x'):
+            try:
+                input = hex(int(input, 16))
+            except ValueError:
+                pass
+        return input
+
+    @commands.command(aliases=['nxerr', 'serr', 'err', 'res'])
+    async def result(self, ctx, err: str):
+        """
+        Parses game console result codes, with a fancy embed. 0x prefix is not required.
+
+        Examples:
+          .err 0xD960D02B
+          .err D960D02B
+          .err 022-2634
+          .err 102-2804
+          .err 2168-0002
+        """
+        err = self.fixup_input(err)
+        system_name, module_name, error, color = self.fetch(err)
+
+        if error:
+            embed = discord.Embed(title=f"Result {err} ({system_name})")
+            embed.add_field(name="Module", value=module_name, inline=False)
+            if error.summary:
+                embed.add_field(name="Summary", value=error.summary, inline=False)
+            if error.level:
+                embed.add_field(name="Level", value=error.level, inline=False)
+            if error.support_url:
+                embed.add_field(name="Further information", value=error.support_url, inline=False)
+                embed.add_field(name="Description", value=error.description, inline=False)
+            embed.color = color
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(f'{ctx.author.mention}, the code you entered is \
+invalid or is for a system I don\'t have support for.', delete_after=10)
+
+def setup(bot):
+    bot.add_cog(Results(bot))
+
