1f532a24e55c4b99448ab113d24a4e1a7a2d07a1
FrozenChen <brobles@umag.cl>
2020-10-15 00:58:48 -0300

    Add new databaseconverter script

--- databaseconverter.py
+++ databaseconverter.py
@@ -1,143 +1,178 @@
-import json
-from configparser import ConfigParser
 import sqlite3
-from sys import exit
+import asyncio
 import discord
-from datetime import datetime
-from discord.ext import commands
-import os
+
+import datetime
+from utils import models
+from utils.models import db
+from configparser import ConfigParser
+
+SQLITE_FILE = './data/kurisu.sqlite'
 
 config = ConfigParser()
-config.read("config.ini")
-
-roles = {
-    'Probation': None,
-    'Muted': None,
-    'No-Help': None,
-    'No-elsewhere': None,
-    'No-art': None,
-    'No-Memes': None,
-    'No-Embed': None,
-    'Small Help': None}
-
-DATABASE_NAME = 'data/kurisu.sqlite'
-bot = commands.Bot(('.', '!'), description="Database Converter!")
+config.read("data/config.ini")
 
 
-@bot.event
-async def on_ready():
-    if not os.path.isfile(DATABASE_NAME):
-        # read schema, open db, init
-        print(f'Creating database {DATABASE_NAME}')
-        with open('schema.sql', 'r', encoding='utf-8') as f:
-            schema = f.read()
-        dbcon = sqlite3.connect(DATABASE_NAME)
-        dbcon.executescript(schema)
-        dbcon.commit()
-        c = dbcon.cursor()
-        print(f'{DATABASE_NAME} initialized')
-    else:
-        # just open db, no setup
-        dbcon = sqlite3.connect(DATABASE_NAME)
-        c = dbcon.cursor()
-
-    fccon = sqlite3.connect('data/fc.sqlite')
-    fc = fccon.cursor()
-    rows = fc .execute('SELECT * FROM friend_codes').fetchall()
-    for row in rows:
-        try:
-            c.execute('INSERT INTO friend_codes VALUES(?, ?)', row)
-        except sqlite3.IntegrityError:
-            continue
-
-    for n in roles.keys():
-        roles[n] = discord.utils.get(bot.guilds[0].roles, name=n)
-
-    with open("data/restrictions.json", "r") as f:
-        rsts = json.load(f)
-    with open("data/warnsv2.json", "r") as f:
-        warns = json.load(f)
-    with open("data/helpers.json", "r") as f:
-        helpers = json.load(f)
-    with open("data/softbans.json", "r") as f:
-        softbans = json.load(f)
-    with open("data/staff.json", "r") as f:
-        staff = json.load(f)
-    with open("data/timenohelp.json", "r") as f:
-        tnohelp = json.load(f)
-    with open("data/timebans.json", "r") as f:
-        timebans = json.load(f)
-    with open("data/timemutes.json", "r") as f:
-        timemutes = json.load(f)
-    with open("data/watch.json", "r") as f:
-        watch = json.load(f)
-
-    # same queries as the database class but no commit until the very end
-    for id in rsts.keys():
-        for rolename in rsts[id]:
-            role = roles[rolename]
-            if c.execute('SELECT user_id FROM permanent_roles WHERE user_id=? AND role_id=?',
-                                  (id, role.id)).fetchone() is None:
-                c.execute ('INSERT INTO permanent_roles VALUES(?, ?)', (id, role.id))
-
-    for id in warns.keys():
-        for warn in warns[id]["warns"]:
-            snowflake = discord.utils.time_snowflake(datetime.strptime(warn['timestamp'],'%Y-%m-%d %H:%M:%S'))
-            if c.execute('SELECT * FROM warns WHERE warn_id=?', (snowflake,)).fetchone():
-                continue
-            c.execute('INSERT INTO warns VALUES(?, ?, ?, ?)', (snowflake, id, warn['issuer_id'], warn['reason']))
-
-    for id in helpers.keys():
-        try:
-            c.execute ('INSERT INTO helpers VALUES(?, ?)', (id, helpers[id]))
-        except sqlite3.IntegrityError:
-            c.execute ('UPDATE helpers SET console=? WHERE user_id=?', (helpers[id], id,))
-
-    for id in softbans.keys():
-        try:
-            c.execute ('INSERT INTO softbans VALUES(?, ? , ?, ?)', (id, softbans[id]['issuer_id'], softbans[id]['reason'], softbans[id]["timestamp"]))
-        except sqlite3.IntegrityError:
-            continue
-
-    for id in staff:
-        try:
-            c.execute ('INSERT INTO staff VALUES(?, ?)', (id, staff[id]))
-        except sqlite3.IntegrityError:
-            c.execute ('UPDATE staff SET position=? WHERE user_id=?', (staff[id], id))
-
-    for id in tnohelp:
-        # assume old warns are older than the ones in the database
-        if c.execute ('SELECT 1 FROM timed_restrictions WHERE user_id=? AND type=?',
-                              (id, 'timenohelp')).fetchone() is None:
-            c.execute ('INSERT INTO timed_restrictions VALUES(?, ?, ?, ?)', (id, tnohelp[id], 'timenohelp', 0))
-
-    for id in timebans:
-        # assume old warns are older than the ones in the database
-        if c.execute ('SELECT 1 FROM timed_restrictions WHERE user_id=? AND type=?',
-                              (id, 'timeban')).fetchone() is None:
-            c.execute ('INSERT INTO timed_restrictions VALUES(?, ?, ?, ?)',
-                               (id, timebans[id], 'timeban', 0))
-
-    for id in timemutes:
-        # assume old warns are older than the ones in the database
-        if c.execute ('SELECT 1 FROM timed_restrictions WHERE user_id=? AND type=?',(id, 'timemute')).fetchone() is None:
-            c.execute ('INSERT INTO timed_restrictions VALUES(?, ?, ?, ?)',(id, timemutes[id], 'timemute', 0))
-
-    for id in watch.keys():
-        try:
-            c.execute ('INSERT INTO watchlist VALUES(?)', (id,))
-        except sqlite3.IntegrityError:
-            continue
-    print('Data converted successfully!')
-    dbcon.commit()
-    dbcon.close()
-    await bot.close()
+def has_seconds(str_timestamp):
+    return "." in str_timestamp.split(":")[2]
 
 
-def main():
-    print('Starting Database Converter')
-    bot.run(config['Main']['token'])
+async def main():
+    await db.set_bind(config['Main']['database_url'])
+    await db.gino.drop_all()
+    await db.gino.create_all()
+    conn = sqlite3.connect(SQLITE_FILE)
+    c = conn.cursor()
+
+    users = []
+
+    # Friend Codes
+    c.execute('SELECT * FROM friend_codes')
+    data = c.fetchall()
+    fc_entries = []
+    if data:
+        for entry in data:
+            fc_entries.append(dict(id=entry[0], fc_3ds=entry[1]))
+            users.append(entry[0])
+
+    # permanent roles
+    c.execute('SELECT * FROM permanent_roles')
+    data = c.fetchall()
+    perm_entries = []
+    roles = []
+    if data:
+        for entry in data:
+            if entry[1] not in roles:
+                roles.append(entry[1])
+            if entry[0] not in users:
+                users.append(entry[0])
+            # There is duplicate entries for some reason
+            if entry not in perm_entries:
+                perm_entries.append(entry)
+        await models.Role.insert().gino.all([dict(id=id) for id in roles])
+
+    # softban
+    c.execute('SELECT * FROM softbans')
+    data = c.fetchall()
+    softban_entries = []
+    if data:
+        for entry in data:
+            softban_entries.append(
+                dict(id=discord.utils.time_snowflake(
+                    datetime.datetime.strptime(entry[3], '%Y-%m-%d %H:%M:%S.%f' if has_seconds(
+                        entry[3]) else '%Y-%m-%d %H:%M:%S')),
+                    user=entry[0], issuer=entry[1],
+                    reason=entry[2]))
+            if entry[0] not in users:
+                users.append(entry[0])
+            if entry[1] not in users:
+                users.append(entry[1])
+
+    # Timed Restricions
+    c.execute('SELECT * FROM timed_restrictions')
+    data = c.fetchall()
+    timeres_entries = []
+    if data:
+        for i, entry in enumerate(data):
+            timeres_entries.append(
+                dict(id=discord.utils.time_snowflake(datetime.datetime.now() + datetime.timedelta(0, i)), user=entry[0],
+                     type=entry[2], end_date=datetime.datetime.strptime(entry[1], '%Y-%m-%d %H:%M:%S.%f' if has_seconds(
+                        entry[1]) else '%Y-%m-%d %H:%M:%S'), alerted=bool(entry[3])))
+            if entry[0] not in users:
+                users.append(entry[0])
+
+    # Warns
+    c.execute('SELECT * FROM warns')
+    data = c.fetchall()
+    warn_entries = []
+    if data:
+        for entry in data:
+            warn_entries.append(
+                dict(id=entry[0], user=entry[1], issuer=entry[2], reason=entry[3]))
+            if entry[1] not in users:
+                users.append(entry[1])
+            if entry[2] not in users:
+                users.append(entry[2])
+
+    # Staff
+    c.execute('SELECT * FROM staff')
+    data = c.fetchall()
+    staff_entries = []
+    staff = []
+    if data:
+        for entry in data:
+            staff_entries.append(dict(id=entry[0], position=entry[1]))
+            staff.append(entry[0])
+            if entry[0] not in users:
+                users.append(entry[0])
+
+    # Helpers
+    c.execute('SELECT * FROM helpers')
+    data = c.fetchall()
+    helper_entries = []
+    if data:
+        for entry in data:
+            try:
+                index = staff.index(entry[0])
+            except:
+                index = -1
+            if index >= 0:
+                staff_entries[index]['console'] = entry[1]
+            else:
+                helper_entries.append(dict(id=entry[0], position='Helper', console=entry[1]))
+                if entry[0] not in users:
+                    users.append(entry[0])
+
+    # channels
+    c.execute('SELECT * FROM nofilter')
+    data = c.fetchall()
+    if data:
+        channel_entries = [dict(id=entry[0], nofilter=True) for entry in data]
+        await models.Channel.insert().gino.all(channel_entries)
+
+    # wordfilter
+    c.execute('SELECT * FROM wordfilter')
+    data = c.fetchall()
+    if data:
+        word_entries = [dict(word=entry[0], kind=entry[1]) for entry in data]
+        await models.FilteredWord.insert().gino.all(word_entries)
+
+    # invitefilter
+    c.execute('SELECT * FROM invitefilter')
+    data = c.fetchall()
+    if data:
+        invite_entries = [dict(code=entry[0], uses=entry[3], alias=entry[2]) for entry in data]
+        await models.ApprovedInvite.insert().gino.all(invite_entries)
+
+    # flags
+    c.execute('SELECT * FROM flags')
+    data = c.fetchall()
+    if data:
+        flag_entries = [dict(name=entry[0], value=bool(entry[1])) for entry in data]
+        await models.Flag.insert().gino.all(flag_entries)
+
+    await models.Member.insert().gino.all([dict(id=id) for id in users])
+    await models.FriendCode.insert().gino.all(fc_entries)
+    await models.Staff.insert().gino.all(staff_entries)
+    await models.TimedRestriction.insert().gino.all(timeres_entries)
+    await models.Warn.insert().gino.all(warn_entries)
+    await models.PermanentRole.insert().gino.all([dict(user_id=user, role_id=role) for user, role in perm_entries])
+    await models.Softban.insert().gino.all(softban_entries)
+    await models.Staff.insert().gino.all(helper_entries)
+
+    # Watchlist
+    c.execute('SELECT * FROM watchlist')
+    data = c.fetchall()
+    mb_entries = []
+    if data:
+
+        async with db.transaction() as tx:
+            for entry in data:
+                if entry[0] in users:
+                    await db.status(f"UPDATE members SET watched='true' where id={entry[0]}")
+                else:
+                    mb_entries.append(dict(id=entry[0], watched=True))
+        await models.Member.insert().gino.all(mb_entries)
 
 
-if __name__=='__main__':
-    exit(main())
+asyncio.get_event_loop().run_until_complete(main())
