da706d522d10cf2736d02d4a23f2246f860b1b64
maximus <mb@maxbeard.pro>
2021-02-07 01:21:35 +0000

    update rule10

--- cogs/rules.py
+++ cogs/rules.py
@@ -102,7 +102,8 @@ class Rules(commands.Cog, command_attrs=dict()):
     @commands.cooldown(rate=1, per=30.0, type=commands.BucketType.channel)
     async def r10(self, ctx):
         """Displays rule 10."""
-        await self.simple_embed(ctx, "Not-safe-for-work content (including gore and other \"shock\" content) is prohibited.", title="Rule 10")
+        await self.simple_embed(ctx, "Not-safe-for-work content (including gore and other \"shock\" content) is prohibited.\n"
+                                "Additionally hornyposting is heavily discouraged in all the channels.", title="Rule 10")
 
     @commands.command()
     @commands.cooldown(rate=1, per=30.0, type=commands.BucketType.channel)
