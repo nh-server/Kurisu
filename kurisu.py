874d750e872a6469477dfeeea6d4b17e44f05421
Ian Burgwin <ian@ianburgwin.net>
2021-02-15 05:10:11 -0800

    push test

--- kurisu.py
+++ kurisu.py
@@ -25,7 +25,7 @@ from utils.manager import WordFilterManager, InviteFilterManager
 from utils.models import db
 from utils.utils import create_error_embed
 
-IS_DOCKER = os.environ.get('IS_DOCKER', 0)
+IS_DOCKER = os.environ.get('IS_DOCKER', '')
 
 # sets working directory to bot's folder
 dir_path = os.path.dirname(os.path.realpath(__file__))
