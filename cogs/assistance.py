2a11f410ed3e6ea20b5c44c113bd84bf481ccd05
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2021-04-11 14:31:15 +0100

    speling

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -478,7 +478,7 @@ and helpers can be found in #welcome-and-rules if you don't know who they are.
 
             If your Switch is **unpatched and can access RCM**:
             Atmosphere and Hekate currently support 12.0.0, and unpatched units will always be hackable.
-            You should follow the precautions in our update guide, and always update Atmosphere and Hekate before upiating the system firmware.
+            You should follow the precautions in our update guide, and always update Atmosphere and Hekate before updating the system firmware.
 
             If your Switch is **hardware patched and cannot access RCM**:
             Stay on the lowest possible firmware version. Any Switch that is patched and above 7.0.1 is unlikely to be hackable.
