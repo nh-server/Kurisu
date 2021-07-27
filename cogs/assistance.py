40efc36d43b9ee87e78ef020b9183455ff6ae989
lifehackerhansol <lifehackerhansol@outlook.com>
2021-07-26 22:46:32 -0700

    because seedminer is safe :kurisusip:

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -468,8 +468,8 @@ and helpers can be found in #welcome-and-rules if you don't know who they are.
             If it is 10.2 or below, please type `.update` in <#261581918653513729> and follow the information there.
 
             If you DO NOT currently have CFW installed (stock console):
-            11.15.0-47 *cannot* be hacked with current methods. ***Updating is not safe***.
-            *Last edited: July 26, 2021*
+            11.15.0-46 can be hacked with current methods. **Updating is safe**; however, if you are on 11.3 or below, it may be worthwhile to stay on that version as it is faster to hack your console there.
+            *Last edited: November 16th, 2020*
             """)
             await ctx.send(embed=embed)
 
