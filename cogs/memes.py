874832bf5ec5d692097de3cc5f7796c1e695ea5d
Rice A Roni <71577598+daderplord@users.noreply.github.com>
2021-09-22 14:56:15 -0700

    Added .arrival (#975)
    * Added .arrival
    
    * changed to 2 randomly chosen gifs , changed to imgur links (thanks hansol for the suggestions :D) and added a hug command
    
    * e
    
    totally didnt forget to add .gif at the end of the links
    
    * Update memes.py
    
    * added .eip aka ei:b: calling me a idiot
    
    yea i think you got the hint

--- cogs/memes.py
+++ cogs/memes.py
@@ -286,6 +286,12 @@ class Memes(commands.Cog):
 
     @commands.command(hidden=True)
     @commands.cooldown(rate=1, per=30.0, type=commands.BucketType.channel)
+    async def eip(self, ctx):
+        """Memes."""
+        await self._meme(ctx, "", imagelink="https://i.imgur.com/SU0Qvc8.png")
+
+    @commands.command(hidden=True)
+    @commands.cooldown(rate=1, per=30.0, type=commands.BucketType.channel)
     async def whoops(self, ctx):
         """Memes."""
         await self._meme(ctx, "", imagelink="https://i.imgur.com/caF9KHk.gif")
@@ -552,6 +558,19 @@ class Memes(commands.Cog):
         """From the amazing Mr. Burguers"""
         departure_gifs = ["https://i.imgur.com/Kbyp7i4.gif", "https://i.imgur.com/Wv8DoGC.gif"]
         await self._meme(ctx, "", imagelink=random.choice(departure_gifs))
+
+    @commands.command(hidden=True)
+    @commands.cooldown(rate=1, per=30.0, type=commands.BucketType.channel)
+    async def arrival(self, ctx):
+        """Glazy can add departure but not arrival smh"""
+        arrival_gifs = ["https://imgur.com/kNlrsth.gif", "https://imgur.com/ZlwaTUp.gif"]
+        await self._meme(ctx, "", imagelink=random.choice(arrival_gifs))
+
+    @commands.command(hidden=True)
+    @commands.cooldown(rate=1, per=30.0, type=commands.BucketType.channel)
+    async def hug(self, ctx, u: discord.Member):
+        """hug"""
+        await self._meme(ctx, f"{self.bot.escape_text(u.display_name)} has received a hug.", True, "https://i.imgur.com/wTHzIXx.jpg")
 
 
 def setup(bot):
