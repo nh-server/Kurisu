0e9645610650baa439098904682e5c821bb71801
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2021-11-04 21:46:54 +0000

    atmosphere 1.2.4

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -20,9 +20,9 @@ class Assistance(commands.Cog, command_attrs=dict(cooldown=commands.Cooldown(1, 
 
     format_map = {
         'nx_firmware': '13.1.0',
-        'ams_ver': '1.2.2',
+        'ams_ver': '1.2.4',
         'hekate_ver': '5.6.5',
-        'last_revision': 'October 31st, 2021',
+        'last_revision': 'November 4th, 2021',
     }
 
     # compatibility until the use of these variables is removed
