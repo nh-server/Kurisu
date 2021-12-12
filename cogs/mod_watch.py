1d90da41cdbb7066d8a90e1be569370b45a8b39f
FrozenChen <brobles@umag.cl>
2021-12-12 19:47:32 -0300

    mod_watch.py: Get member from guild if possible

--- cogs/mod_watch.py
+++ cogs/mod_watch.py
@@ -53,7 +53,11 @@ class Modwatch(commands.Cog):
     @commands.command()
     async def listwatch(self, ctx):
         watchlist = await crud.get_watch_list()
-        messages = wrap("\n".join(f"<@{member.id}> ({member.id})" for member in watchlist), 1810, break_long_words=False, replace_whitespace=False)
+        lines = []
+        for db_member in watchlist:
+            member = ctx.guild.get_member(db_member.id)
+            lines.append(f"{f'{member.name}' if member else f'<@{db_member.id}>'} ({db_member.id})")
+        messages = wrap('\n'.join(lines), 1810, break_long_words=False, replace_whitespace=False)
         if not messages:
             await ctx.send("The watchlist is empty!")
             return
