1cf9139f0b7536913dc092dd33a417bae83749b1
FrozenChen <brobles@umag.cl>
2022-07-03 06:05:22 -0300

    checks.py: Fix check for helpers

--- utils/checks.py
+++ utils/checks.py
@@ -30,7 +30,11 @@ def is_staff_app(role: str):
 
 
 def check_staff(bot: 'Kurisu', role: str, user_id: int) -> bool:
-    position = bot.configuration.staff.get(user_id)
+    if bot.configuration.helpers.get(user_id):
+        position = StaffRank.Helper
+    else:
+        position = bot.configuration.staff.get(user_id)
+
     if position is None:
         return False
     return position <= StaffRank[role]
