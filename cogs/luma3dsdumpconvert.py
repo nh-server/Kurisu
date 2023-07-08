a7df075dd7184e56c93a72130b71c4b6c9312e64
FrozenChen <brobles@umag.cl>
2023-07-08 19:58:05 -0300

    Use snake case

--- cogs/luma3dsdumpconvert.py
+++ cogs/luma3dsdumpconvert.py
@@ -10,10 +10,10 @@ if TYPE_CHECKING:
     from kurisu import Kurisu
 
 # adapted from luma3ds_exception_dump_parser
-handledExceptionNames = ("FIQ", "undefined instruction", "prefetch abort", "data abort")
-registerNames = tuple("r{0}".format(i) for i in range(13)) + ("sp", "lr", "pc", "cpsr") + ("dfsr", "ifsr", "far") + ("fpexc", "fpinst", "fpinst2")
-svcBreakReasons = ("(svcBreak: panic)", "(svcBreak: assertion failed)", "(svcBreak: user-related)")
-faultStatusSources = {
+handled_exception_names = ("FIQ", "undefined instruction", "prefetch abort", "data abort")
+register_names = tuple("r{0}".format(i) for i in range(13)) + ("sp", "lr", "pc", "cpsr") + ("dfsr", "ifsr", "far") + ("fpexc", "fpinst", "fpinst2")
+svc_break_reasons = ("(svcBreak: panic)", "(svcBreak: assertion failed)", "(svcBreak: user-related)")
+fault_status_sources = {
     0b1: 'Alignment', 0b100: 'Instruction cache maintenance operation fault',
     0b1100: 'External Abort on translation - First-level', 0b1110: 'External Abort on translation - Second-level',
     0b101: 'Translation - Section', 0b111: 'Translation - Page', 0b11: 'Access bit - Section', 0b110: 'Access bit - Page',
@@ -36,66 +36,66 @@ class Luma3DSDumpConvert(commands.Cog):
         if unpack_from("<2I", data) != (0xdeadc0de, 0xdeadcafe):
             return "Invalid file format"
 
-        version, processor, exceptionType, _, nbRegisters, codeDumpSize, stackDumpSize, additionalDataSize = unpack_from("<8I", data, 8)
-        nbRegisters //= 4
+        version, processor, exception_type, _, nb_registers, code_dump_size, stack_dump_size, additional_data_size = unpack_from("<8I", data, 8)
+        nb_registers //= 4
 
-        processor, coreId = processor & 0xffff, processor >> 16
+        processor, core_id = processor & 0xffff, processor >> 16
 
         if version < (1 << 16) | 2:
             return "Incompatible format version"
 
-        registers = unpack_from("<{0}I".format(nbRegisters), data, 40)
-        codeOffset = 40 + 4 * nbRegisters
-        codeDump = data[codeOffset:codeOffset + codeDumpSize]
-        stackOffset = codeOffset + codeDumpSize
+        registers = unpack_from("<{0}I".format(nb_registers), data, 40)
+        code_offset = 40 + 4 * nb_registers
+        code_dump = data[code_offset:code_offset + code_dump_size]
+        stack_offset = code_offset + code_dump_size
         # stackDump = data[stackOffset:stackOffset + stackDumpSize]
-        addtionalDataOffset = stackOffset + stackDumpSize
-        additionalData = data[addtionalDataOffset:addtionalDataOffset + additionalDataSize]
+        addtional_data_offset = stack_offset + stack_dump_size
+        additional_data = data[addtional_data_offset:addtional_data_offset + additional_data_size]
 
         if processor == 9:
             out_message += "Processor: Arm9\n"
         else:
-            out_message += "Processor: Arm11 (core {0})\n".format(coreId)
+            out_message += "Processor: Arm11 (core {0})\n".format(core_id)
 
-        typeDetailsStr = ""
-        if exceptionType == 2:
-            if (registers[16] & 0x20) == 0 and codeDumpSize >= 4:
-                instr = unpack_from("<I", codeDump[-4:])[0]
+        type_details_str = ""
+        if exception_type == 2:
+            if (registers[16] & 0x20) == 0 and code_dump_size >= 4:
+                instr = unpack_from("<I", code_dump[-4:])[0]
                 if instr == 0xe12fff7e:
-                    typeDetailsStr = " (kernel panic)"
+                    type_details_str = " (kernel panic)"
                 elif instr == 0xef00003c:
-                    typeDetailsStr = " " + (svcBreakReasons[registers[0]] if registers[0] < 3 else "(svcBreak)")
-            elif (registers[16] & 0x20) == 1 and codeDumpSize >= 2:
-                instr = unpack_from("<I", codeDump[-4:])[0]
+                    type_details_str = " " + (svc_break_reasons[registers[0]] if registers[0] < 3 else "(svcBreak)")
+            elif (registers[16] & 0x20) == 1 and code_dump_size >= 2:
+                instr = unpack_from("<I", code_dump[-4:])[0]
                 if instr == 0xdf3c:
-                    typeDetailsStr = " " + (svcBreakReasons[registers[0]] if registers[0] < 3 else "(svcBreak)")
+                    type_details_str = " " + (svc_break_reasons[registers[0]] if registers[0] < 3 else "(svcBreak)")
 
         elif processor != 9 and (registers[20] & 0x80000000) != 0:
-            typeDetailsStr = " (VFP exception)"
+            type_details_str = " (VFP exception)"
 
-        out_message += "Exception type: {0}{1}\n".format("unknown" if exceptionType >= len(handledExceptionNames) else handledExceptionNames[exceptionType], typeDetailsStr)
+        out_message += "Exception type: {0}{1}\n".format("unknown" if exception_type >= len(handled_exception_names) else handled_exception_names[exception_type], type_details_str)
 
-        if processor == 11 and exceptionType >= 2:
-            xfsr = registers[18] if exceptionType == 2 else registers[17]
-            out_message += "Fault status: " + faultStatusSources[xfsr & 0xf] + "\n"
+        if processor == 11 and exception_type >= 2:
+            xfsr = registers[18] if exception_type == 2 else registers[17]
+            out_message += "Fault status: " + fault_status_sources[xfsr & 0xf] + "\n"
 
-        if additionalDataSize != 0:
+        if additional_data_size != 0:
             if processor == 11:
-                out_message += "Current process: {0} ({1:016x})\n".format(additionalData[:8].decode("ascii"), unpack_from("<Q", additionalData, 8)[0])
+                out_message += "Current process: {0} ({1:016x})\n".format(additional_data[:8].decode("ascii"), unpack_from("<Q", additional_data, 8)[0])
             else:
-                out_message += "Arm9 RAM dump exist, size {0:x}\n".format(additionalDataSize)
+                out_message += "Arm9 RAM dump exist, size {0:x}\n".format(additional_data_size)
 
         out_message += "\nRegister dump:\n"
-        for i in range(0, nbRegisters - (nbRegisters % 2), 2):
+        for i in range(0, nb_registers - (nb_registers % 2), 2):
             if i == 16:
                 out_message += "\n"
-            out_message += "{0:<8}{1:<12}{2:<8}{3:<12}\n".format(registerNames[i], "{0:08x}".format(registers[i]), registerNames[i+1], "{0:08x}".format(registers[i+1]))
-        if nbRegisters % 2 == 1:
-            if processor == 11 and exceptionType == 3:
+            out_message += "{0:<8}{1:<12}{2:<8}{3:<12}\n".format(register_names[i], "{0:08x}".format(registers[i]), register_names[i + 1], "{0:08x}".format(registers[i + 1]))
+        if nb_registers % 2 == 1:
+            if processor == 11 and exception_type == 3:
                 access_type = "Write" if registers[17] & (1 << 11) != 0 else "Read"
-                out_message += "{0:<8}{1:<12}Access type: {2}\n".format(registerNames[nbRegisters - 1], "{0:08x}".format(registers[nbRegisters - 1]), access_type)
+                out_message += "{0:<8}{1:<12}Access type: {2}\n".format(register_names[nb_registers - 1], "{0:08x}".format(registers[nb_registers - 1]), access_type)
             else:
-                out_message += "{0:<8}{1:<12}\n".format(registerNames[nbRegisters - 1], "{0:08x}".format(registers[nbRegisters - 1]))
+                out_message += "{0:<8}{1:<12}\n".format(register_names[nb_registers - 1], "{0:08x}".format(registers[nb_registers - 1]))
 
         return out_message
 
