9b3d46e9689e1a92ffc24e7251ca23bef4ae7900
eip <36315290+eip618@users.noreply.github.com>
2023-11-28 11:22:32 +1100

    mfw "

--- cogs/memes.py
+++ cogs/memes.py
@@ -567,7 +567,7 @@ class Memes(commands.Cog):
         """Glazy can add departure but not arrival smh"""
         arrival_gifs = ["https://nintendohomebrew.com/assets/img/nhmemes/arrival1.gif",
                         "https://nintendohomebrew.com/assets/img/nhmemes/arrival2.png",
-                         https://nintendohomebrew.com/assets/img/nhmemes/arrival3.gif"]
+                        "https://nintendohomebrew.com/assets/img/nhmemes/arrival3.gif"]
         await self._meme(ctx, "", image_link=random.choice(arrival_gifs))
 
     @commands.command(hidden=True)
