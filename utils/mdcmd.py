a4d8951635ac7691a8ca4bc26670cb31b14a9347
DannyAAM <danny@saru.moe>
2024-01-21 03:01:44 +0800

    fix wiiu wii detection

--- utils/mdcmd.py
+++ utils/mdcmd.py
@@ -154,7 +154,7 @@ def check_console(message, channel, consoles):
         consoles = (consoles,)
     if message in consoles:
         return True
-    elif channel.startswith(consoles) and message not in systems:
+    elif channel.startswith(tuple(map(lambda c: f"{c}-", consoles))) and message not in systems:
         return True
     return False
 
