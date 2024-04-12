a84c9c357d4e2956d48ba6d22358558985d349a5
Jade <48196637+GlazedBelmont@users.noreply.github.com>
2024-04-12 12:16:59 -0400

    Add email notice to ban messages

--- cogs/kickban.py
+++ cogs/kickban.py
@@ -117,7 +117,8 @@ class KickBan(commands.GroupCog):
         msg = f"You were banned from {interaction.guild.name}."
         if reason:
             msg += " The given reason is: " + reason
-
+        msg += "If you think this is a mistake, contact frozenchen on discord or send a email to staff@nintendohomebrew.com"
+                                   
         if duration is not None:
             timestamp = datetime.datetime.now(self.bot.tz)
             delta = datetime.timedelta(seconds=duration)
@@ -224,6 +225,7 @@ class KickBan(commands.GroupCog):
             if reason:
                 msg += " The given reason is: " + reason
             msg += f"\n\nThis ban lasts until {unban_time_string}."
+            msg += "If you think this is a mistake, contact frozenchen on discord or send a email to staff@nintendohomebrew.com"
             await send_dm_message(member, msg, ctx)
         try:
             await ctx.guild.ban(member, reason=reason, delete_message_days=0)
@@ -283,7 +285,7 @@ class KickBan(commands.GroupCog):
             await self.filters.add_filtered_word(site, FilterKind.ScammingSite)
             await self.bot.channels['mod-logs'].send(f"🆕 **Added**: {ctx.author.mention} added `{site}` to the word filter!")
         ban_msg = ("You have been banned from Nintendo Homebrew for linking scamming sites in the server."
-                   "If you think this is a mistake contact ❅FrozenFire❆#0700 on discord or send a email to staff@nintendohomebrew.com")
+                   "If you think this is a mistake, contact frozenchen on discord or send a email to staff@nintendohomebrew.com")
         await send_dm_message(member, ban_msg)
         reason = "Linking scamming site"
         await member.ban(reason=reason, delete_message_days=1)
