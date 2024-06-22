823fb809dbfcc3aee90a2d082a00cde8d19bc6a7
eip <36315290+eip618@users.noreply.github.com>
2024-06-22 20:06:10 +1000

    change order of operations

--- cogs/assistance.py
+++ cogs/assistance.py
@@ -180,10 +180,10 @@ class Assistance(commands.GroupCog):
         for channel in channels:
             if channel not in self.soaps_category.channels:
                 continue
-        await channel.delete()
         msg = f":x: **:soap: channel deleted**: {ctx.author.mention} deleted :soap: channel {channel.mention} | {channel.name} ({channel.id})"
         await self.bot.channels['mod-logs'].send(msg)
         await ctx.send(f"Deleted :soap: {channel.mention}.")
+        await channel.delete()
 
     @is_staff('OP')
     @commands.guild_only()
