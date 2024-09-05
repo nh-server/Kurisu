99d749bb6f61344783ad06e6f8dacf1ec4402f42
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-04 19:06:17 -0700

    wiierror: Add a basic cache for Wiimmfi responses
    This cache will only be alive during runtime; it is not using the db.
    
    It (hopefully) saves us from too many API calls.

--- cogs/assistancewii.py
+++ cogs/assistancewii.py
@@ -27,6 +27,9 @@ class AssistanceWii(commands.GroupCog):
     def __init__(self, bot: Kurisu):
         self.bot: Kurisu = bot
 
+    # Cached error codes from Wiimmfi
+    saved_errors = []
+
     @commands.dynamic_cooldown(KurisuCooldown(1, 30.0), commands.BucketType.channel)
     @commands.command(aliases=["wfcerror"])
     async def wiierror(self, ctx: KurisuContext, error: int):
@@ -38,19 +41,33 @@ class AssistanceWii(commands.GroupCog):
         if error > 999999:
             return await ctx.send(f"{ctx.author.mention} This is an invalid error code. Please try again.")
         apicall = f"https://wiimmfi.de/error?m=json&e={error}"
-        async with ctx.typing():
-            async with self.bot.session.get(apicall) as r:
-                if r.status == 200:
-                    response = await r.json()
-                    if response[0]["found"] == 0:
-                        return await ctx.send(f"{ctx.author.mention} This error code does not exist.")
+
+        errorinfo = {}
+
+        # Check if we have this error cached
+        for i in self.saved_errors:
+            if i["error"] == error:
+                errorinfo = i
+
+        # If our error wasn't cached, go ask Wiimmfi
+        if not errorinfo:
+            async with ctx.typing():
+                async with self.bot.session.get(apicall) as r:
+                    if r.status == 200:
+                        response = await r.json()
+                        errorinfo = response[0]
+                        # Cache the error
+                        self.saved_errors.append(response[0])
                     else:
-                        embed = discord.Embed(title=f"Error {error}", colour=ConsoleColor.wii())
-                        for i in response[0]["infolist"]:
-                            embed.add_field(name=i["type"], value=f'**{i["name"]}**: {markdownify(i["info"])}', inline=False)
-                        return await ctx.send(embed=embed)
-                else:
-                    return await ctx.send(f'{ctx.author.mention} API returned error {r.status}. Please check your values and try again.')
+                        return await ctx.send(f'{ctx.author.mention} API returned error {r.status}. Please check your values and try again.')
+
+        if errorinfo["found"] == 0:
+            return await ctx.send(f"{ctx.author.mention} This error code does not exist.")
+        else:
+            embed = discord.Embed(title=f"Error {error}", colour=ConsoleColor.wii())
+            for i in errorinfo["infolist"]:
+                embed.add_field(name=i["type"], value=f'**{i["name"]}**: {markdownify(i["info"])}', inline=False)
+            return await ctx.send(embed=embed)
 
 
 add_md_files_as_commands(AssistanceWii, console_cmd="wii")
