9cb09cd2ad302cc629ddde209507478f5a9f161c
eip <36315290+eip618@users.noreply.github.com>
2025-01-06 19:40:29 +1100

    add remote submission kick to soap opener

--- cogs/soap.py
+++ cogs/soap.py
@@ -88,7 +88,10 @@ class Soap(commands.Cog):
                            "    Unscrew them until they click, then gently lift the backplate using the notches at the sides.\n"
                            "  - The label also may be under the battery.\n"
                            "  - If you still can't find it, please request further assistance.\n"
-                           "10. Please wait for further instructions.")
+                           "10. Please wait for further instructions.\n"
+                           "### Don't have access to a computer?\n"
+                           "There is a way to submit your `essential.exefs` using only your hacked 3DS and a WiFi connection, but it's _not preferred_ compared to the above instructions.\n"
+                           "If you need more information about this remote submission method, please let us know in this channel.")
         await self.bot.channels['mod-logs'].send(f"⭕️ **🧼 access granted**: {ctx.author.mention} granted access to 🧼 channel to {helpee.mention}")
         msg = f"🆕 **🧼 channel created**: {ctx.author.mention} created 🧼 channel {channel.mention} | {channel.name} ({channel.id})"
         await self.bot.channels['mod-logs'].send(msg)
