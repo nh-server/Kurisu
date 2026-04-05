a5d78eb620e29e3361764da8d7fbfeff7b0ffd06
FrozenChen <brobles@umag.cl>
2026-04-05 20:05:24 -0300

    Delete messages from users kicked by the killbox

--- cogs/events.py
+++ cogs/events.py
@@ -421,6 +421,7 @@ class Events(commands.Cog):
                         self.bot.actions.append(f'wk:{message.author.id}')
                         await message.author.kick(reason=f"Kill box automated Action. See {mes.jump_url}")
                         await message.delete()
+                        await self.bot.server_logs.purge_user_messages(message.author.id, 10, after=discord.utils.utcnow() - datetime.timedelta(days=1))
                     except discord.Forbidden:
                         self.bot.actions.remove(f'wk:{message.author.id}')
                     return
